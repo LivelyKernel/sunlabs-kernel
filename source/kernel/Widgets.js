12a7a474e960d9f8bc0ab2bcc282e07ba6565c5a
Antero Taivalsaari <antero.taivalsaari@nokia.com>
2007-08-30 08:56:31 +0000

    Moved samplemorphs to Main from Widgets. Also removed createprototypeworld, which is not needed anymore

--- source/kernel/Widgets.js
+++ source/kernel/Widgets.js
@@ -1724,89 +1724,15 @@ Object.extend(WorldMorph, {
         return w;
     },
 
+    // NOT needed since main calls createEmptyWorld and samples have been moved to main.js
     // Set up a world with morphs to copy
     // FIXME: This is sample code and should be moved elsewhere!
-    createPrototypeWorld: function() {
-        var widget, zzRect, zzEll, zzLine, zzPoly, zzText, zzScript; // Comment to make these global for debugging access
+    /*createPrototypeWorld: function() {
+       
         var w = this.createEmptyWorld();
-
-        var colors = Color.wheel(4);
-        var loc = pt(30,450); 
-        var widgetExtent = pt(70, 30);
-        var dy = pt(0,50); 
-        var dx = pt(120,0);
- 
-        // Create a sample rectangle       
-        widget = Morph(loc.extent(widgetExtent), "rect");
-
-        widget.setFill(colors[0]);
-        w.addMorph(widget);
-        zzRect = widget;
-
-        //widget = Morph.fromMarkup('<g type="Morph" transform="translate(210,395)"><ellipse cx="0" cy="0" rx="35" ry="15" type="ellicapse" fill="rgb(98,179,17)" stroke-width="1" stroke="rgb(0,0,0)"/><g type="Submorphs"/></g>');
-
-        // Create a sample ellipse
-        widget = Morph(loc.addPt(dx).extent(widgetExtent), "ellipse");
-        widget.setFill(colors[1]);
-
-        w.addMorph(widget);
-        zzEll = widget;
-
-        // Create a sample line
-        loc = loc.addPt(dy);
-        widget = Morph.makeLine([loc.addXY(0,15),loc.addXY(70,15)], 2, Color.black);
-        w.addMorph(widget);
-        zzLine = widget;
-        var l2 = loc.addPt(dx);
-        
-        // Create a sample polygon
-        widget = Morph(l2.asRectangle(),"rect");
-        widget.setShape(PolygonShape(null, [pt(0,0),pt(70,0),pt(40,30),pt(0,0)],
-                        colors[2],1,Color.black));
-        w.addMorph(widget);
-        zzPoly = widget;
-        loc = loc.addPt(dy);    
-        
-        // Create sample text widgets
-        var showBigText = true;
-        if (showBigText) {
-            widget = TextMorph(loc.extent(pt(100,50)),"Big Text"); // big text
-            widget.setFontSize(20);
-            widget.setTextColor(Color.blue);
-            w.addMorph(widget);
-            zzText = widget;
-            widget = TextMorph(loc.addPt(dx).extent(pt(140,50)),"Unbordered"); // unbordered text
-            widget.setFontSize(20);  
-            widget.setBorderWidth(0);  
-            widget.setFill(null);
-            w.addMorph(widget); 
-        }
-            
-        // Sample executable script pane
-        var showPenScript = true;
-        if (showPenScript) {
-            widget = TextMorph(pt(50,30).extent(pt(250,50)), Pen.script);
-            widget.align(widget.bounds().bottomRight(), w.bounds().topRight().addPt(pt(-50,100))); 
-            w.addMorph(widget);
-            zzScript = widget; 
-        }
-
-        if (Config.showTestText) {
-            Pen.script = "gggggggggg " +
-            "hhhhhhhhhh\n" +
-            "iiiiiiiiii " +
-            "jjjjjjjjjj\n" +
-            "kkkkkkkkkk " +
-            "llllllllll\n" +
-            "mmmmmmmmmm " +
-            "nnnnnnnnnn\n";
-            widget = TestTextMorph(pt(50,50).extent(pt(250,50)), Pen.script);
-            w.addMorph(widget);
-            widget.setScale(3.0);
-        }
         
         return w; 
-    },
+    },*/
 
     // Default styles for the style manager    
     defaultThemes: { // --Style architecture is under construction!--
