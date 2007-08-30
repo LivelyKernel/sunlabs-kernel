f1af074308c2a6bdfbf690ec7931ce6251537622
Antero Taivalsaari <antero.taivalsaari@nokia.com>
2007-08-30 12:10:04 +0000

    Moving samplemorphs caused some examples to disappear. I rearranged the code a bit and it seems to fixed the problem.

--- source/kernel/Main.js
+++ source/kernel/Main.js
@@ -9,7 +9,85 @@ Config.shiftDragForDup = true; // easy duplicate gesture
 
 WorldMorph.populateWithExamples = function(world, otherWorld, server) {
 
-    var widget; 
+    //var widget; 
+    //Examplemorphs moved here from widgets.js
+    var widget, zzRect, zzEll, zzLine, zzPoly, zzText, zzScript; // Comment to make these global for debugging access
+    var showSampleMorphs = true;
+    if (showSampleMorphs){
+      var colors = Color.wheel(4);
+      var loc = pt(30,450); 
+      var widgetExtent = pt(70, 30);
+      var dy = pt(0,50); 
+      var dx = pt(120,0);
+      // Create a sample rectangle       
+      widget = Morph(loc.extent(widgetExtent), "rect");
+  
+      widget.setFill(colors[0]);
+      world.addMorph(widget);
+      zzRect = widget;
+  
+      //widget = Morph.fromMarkup('<g type="Morph" transform="translate(210,395)"><ellipse cx="0" cy="0" rx="35" ry="15" type="ellicapse" fill="rgb(98,179,17)" stroke-width="1" stroke="rgb(0,0,0)"/><g type="Submorphs"/></g>');
+  
+      // Create a sample ellipse
+      widget = Morph(loc.addPt(dx).extent(widgetExtent), "ellipse");
+      widget.setFill(colors[1]);
+  
+      world.addMorph(widget);
+      zzEll = widget;
+  
+      // Create a sample line
+      loc = loc.addPt(dy);
+      widget = Morph.makeLine([loc.addXY(0,15),loc.addXY(70,15)], 2, Color.black);
+      world.addMorph(widget);
+      zzLine = widget;
+      var l2 = loc.addPt(dx);
+      
+      // Create a sample polygon
+      widget = Morph(l2.asRectangle(),"rect");
+      widget.setShape(PolygonShape(null, [pt(0,0),pt(70,0),pt(40,30),pt(0,0)],
+                      colors[2],1,Color.black));
+      world.addMorph(widget);
+      zzPoly = widget;
+      loc = loc.addPt(dy);    
+      
+      // Create sample text widgets
+      var showBigText = true;
+      if (showBigText) {
+          widget = TextMorph(loc.extent(pt(100,50)),"Big Text"); // big text
+          widget.setFontSize(20);
+          widget.setTextColor(Color.blue);
+          world.addMorph(widget);
+          zzText = widget;
+          widget = TextMorph(loc.addPt(dx).extent(pt(140,50)),"Unbordered"); // unbordered text
+          widget.setFontSize(20);  
+          widget.setBorderWidth(0);  
+          widget.setFill(null);
+          world.addMorph(widget); 
+      }
+          
+      // Sample executable script pane
+      var showPenScript = true;
+      if (showPenScript) {
+          widget = TextMorph(pt(50,30).extent(pt(250,50)),Pen.script);
+          widget.align(widget.bounds().bottomRight(), world.bounds().topRight().addPt(pt(-50,100))); 
+          world.addMorph(widget);
+          zzScript = widget; 
+      }
+  
+      if (Config.showTestText) {
+          Pen.script = "gggggggggg " +
+          "hhhhhhhhhh\n" +
+          "iiiiiiiiii " +
+          "jjjjjjjjjj\n" +
+          "kkkkkkkkkk " +
+          "llllllllll\n" +
+          "mmmmmmmmmm " +
+          "nnnnnnnnnn\n";
+          widget = TestTextMorph(pt(50,50).extent(pt(250,50)), Pen.script);
+          world.addMorph(widget);
+          widget.setScale(3.0);
+      }
+    }
     // zzHand = world.worldState.hands[0];
     var showStar = true;
     if (showStar) {  // Make a star
@@ -187,84 +265,7 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
     var showMessenger = !Config.skipMostExamples;
     if (showMessenger) new MessengerWidget().openIn(world, pt(30, 600));
   
-    //Examplemorphs moved here from widgets.js
-    var showSampleMorphs = true;
-    if (showSampleMorphs){
-      var widget, zzRect, zzEll, zzLine, zzPoly, zzText, zzScript; // Comment to make these global for debugging access
-      var colors = Color.wheel(4);
-      var loc = pt(30,450); 
-      var widgetExtent = pt(70, 30);
-      var dy = pt(0,50); 
-      var dx = pt(120,0);
-      // Create a sample rectangle       
-      widget = Morph(loc.extent(widgetExtent), "rect");
-  
-      widget.setFill(colors[0]);
-      world.addMorph(widget);
-      zzRect = widget;
-  
-      //widget = Morph.fromMarkup('<g type="Morph" transform="translate(210,395)"><ellipse cx="0" cy="0" rx="35" ry="15" type="ellicapse" fill="rgb(98,179,17)" stroke-width="1" stroke="rgb(0,0,0)"/><g type="Submorphs"/></g>');
-  
-      // Create a sample ellipse
-      widget = Morph(loc.addPt(dx).extent(widgetExtent), "ellipse");
-      widget.setFill(colors[1]);
-  
-      world.addMorph(widget);
-      zzEll = widget;
-  
-      // Create a sample line
-      loc = loc.addPt(dy);
-      widget = Morph.makeLine([loc.addXY(0,15),loc.addXY(70,15)], 2, Color.black);
-      world.addMorph(widget);
-      zzLine = widget;
-      var l2 = loc.addPt(dx);
-      
-      // Create a sample polygon
-      widget = Morph(l2.asRectangle(),"rect");
-      widget.setShape(PolygonShape(null, [pt(0,0),pt(70,0),pt(40,30),pt(0,0)],
-                      colors[2],1,Color.black));
-      world.addMorph(widget);
-      zzPoly = widget;
-      loc = loc.addPt(dy);    
-      
-      // Create sample text widgets
-      var showBigText = true;
-      if (showBigText) {
-          widget = TextMorph(loc.extent(pt(100,50)),"Big Text"); // big text
-          widget.setFontSize(20);
-          widget.setTextColor(Color.blue);
-          world.addMorph(widget);
-          zzText = widget;
-          widget = TextMorph(loc.addPt(dx).extent(pt(140,50)),"Unbordered"); // unbordered text
-          widget.setFontSize(20);  
-          widget.setBorderWidth(0);  
-          widget.setFill(null);
-          world.addMorph(widget); 
-      }
-          
-      // Sample executable script pane
-      var showPenScript = true;
-      if (showPenScript) {
-          widget = TextMorph(pt(50,30).extent(pt(250,50)),Pen.script);
-          widget.align(widget.bounds().bottomRight(), world.bounds().topRight().addPt(pt(-50,100))); 
-          world.addMorph(widget);
-          zzScript = widget; 
-      }
-  
-      if (Config.showTestText) {
-          Pen.script = "gggggggggg " +
-          "hhhhhhhhhh\n" +
-          "iiiiiiiiii " +
-          "jjjjjjjjjj\n" +
-          "kkkkkkkkkk " +
-          "llllllllll\n" +
-          "mmmmmmmmmm " +
-          "nnnnnnnnnn\n";
-          widget = TestTextMorph(pt(50,50).extent(pt(250,50)), Pen.script);
-          world.addMorph(widget);
-          widget.setScale(3.0);
-      }
-    }
+    
 
     return world;
 }
