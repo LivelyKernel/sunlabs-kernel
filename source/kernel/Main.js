5756cf72fe75fca6c3c4f634262f2880836451c0
Antero Taivalsaari <antero.taivalsaari@nokia.com>
2007-09-05 12:36:06 +0000

    Restructured the current demo worlds. Moved all the simple stuff away from the main display.

--- source/kernel/Main.js
+++ source/kernel/Main.js
@@ -6,87 +6,10 @@ var Global = this;
 
 Config.shiftDragForDup = true; // easy duplicate gesture
 
-WorldMorph.populateWithExamples = function(world, otherWorld, server) {
+function populateWorldWithExamples(world, otherWorld, server) {
 
-    //var widget; 
-    //Examplemorphs moved here from widgets.js
-    var widget, zzRect, zzEll, zzLine, zzPoly, zzText, zzScript; // Comment to make these global for debugging access
-    var showSampleMorphs = true;
-    if (showSampleMorphs){
-        var colors = Color.wheel(4);
-        var loc = pt(30,450); 
-        var widgetExtent = pt(70, 30);
-        var dy = pt(0,50); 
-        var dx = pt(120,0);
-     
-        // Create a sample rectangle       
-        widget = Morph(loc.extent(widgetExtent), "rect");
-        widget.setFill(colors[0]);
-        world.addMorph(widget);
-        zzRect = widget;
-  
-        //widget = Morph.fromMarkup('<g type="Morph" transform="translate(210,395)"><ellipse cx="0" cy="0" rx="35" ry="15" type="ellicapse" fill="rgb(98,179,17)" stroke-width="1" stroke="rgb(0,0,0)"/><g type="Submorphs"/></g>');
-  
-        // Create a sample ellipse
-        widget = Morph(loc.addPt(dx).extent(widgetExtent), "ellipse");
-        widget.setFill(colors[1]);
-        world.addMorph(widget);
-        zzEll = widget;
-  
-        // Create a sample line
-        loc = loc.addPt(dy);
-        widget = Morph.makeLine([loc.addXY(0,15),loc.addXY(70,15)], 2, Color.black);
-        world.addMorph(widget);
-        zzLine = widget;
-      
-        // Create a sample polygon
-        var l2 = loc.addPt(dx);
-        widget = Morph(l2.asRectangle(),"rect");
-        widget.setShape(PolygonShape([pt(0,0),pt(70,0),pt(40,30),pt(0,0)], colors[2],1,Color.black));
-        world.addMorph(widget);
-        zzPoly = widget;
-        loc = loc.addPt(dy);    
-      
-        // Create sample text widgets
-        var showBigText = true;
-        if (showBigText) {
-            widget = TextMorph(loc.extent(pt(100,50)),"Big Text"); // big text
-            widget.setFontSize(20);
-            widget.setTextColor(Color.blue);
-            world.addMorph(widget);
-            zzText = widget;
-            widget = TextMorph(loc.addPt(dx).extent(pt(140,50)),"Unbordered"); // unbordered text
-            widget.setFontSize(20);  
-            widget.setBorderWidth(0);  
-            widget.setFill(null);
-            world.addMorph(widget); 
-        }
-          
-        // Sample executable script pane
-        var showPenScript = true;
-        if (showPenScript) {
-            widget = TextMorph(pt(50,30).extent(pt(250,50)),Pen.script);
-            widget.align(widget.bounds().bottomRight(), world.bounds().topRight().addPt(pt(-50,100))); 
-            world.addMorph(widget);
-            zzScript = widget; 
-        }
+    var widget;
 
-        if (Config.showTestText) {
-            Pen.script = "gggggggggg " +
-            "hhhhhhhhhh\n" +
-            "iiiiiiiiii " +
-            "jjjjjjjjjj\n" +
-            "kkkkkkkkkk " +
-            "llllllllll\n" +
-            "mmmmmmmmmm " +
-            "nnnnnnnnnn";
-            widget = TestTextMorph(pt(50,50).extent(pt(250,50)), Pen.script);
-            world.addMorph(widget);
-            widget.setScale(3.0);
-        }
-    }
-   
-    // zzHand = world.worldState.hands[0];
     var showStar = true;
     if (showStar) {  // Make a star
         var makeStarVertices = function(r,center,startAngle) {
@@ -105,11 +28,11 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
         widget = Morph(pt(0,0).asRectangle(), "rect");
         widget.setShape(PolygonShape(makeStarVertices(50,pt(0,0),0), Color.yellow, 1, Color.black));
         // makeGradient(Color.yellow, Color.yellow.lighter().lighter()));
-        widget.setPosition(pt(320, 480));
+        widget.setPosition(pt(135, 360));
         world.addMorph(widget);
             
         var spinningStar = !Config.skipMostExamples || Config.spinningStar;
-        if(spinningStar) {  // Make the star spin as a test of stepping
+        if (spinningStar) {  // Make the star spin as a test of stepping
             if (Config.useNewScheduler) {
                 widget.startStepping(1/20, "rotateBy", 0.1);
             } else {
@@ -121,7 +44,7 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
 
     var showClock = true;
     if (showClock) {
-        widget = ClockMorph(pt(500, 525), 50);
+        widget = ClockMorph(pt(65, 410), 50);
         // clockWidget.addClipRect(Rectangle(20,20,80,80));
         world.addMorph(widget);
         if (Config.useNewScheduler) {
@@ -151,38 +74,96 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
         gameMorph.runAsteroidsGame();
     }
     
+    // Sample executable script pane
+    var showPenScript = true;
+    if (showPenScript) {
+        widget = TextMorph(pt(50,30).extent(pt(250,50)),Pen.script);
+        widget.align(widget.bounds().bottomRight(), world.bounds().topRight().addPt(pt(-50,100))); 
+        world.addMorph(widget);
+    }
+
+    // example icon morph with a fisheye effect 'on'
+    var showIcon = !Config.skipMostExamples;
+    if (showIcon) {
+        // maybe the icons should have a rectangle shaped images (unlike here)
+        //var icon = ImageMorph(Rectangle(30, 360, 80, 50), "http://logos.sun.com/images/SunSample.gif");
+        var icon = ImageMorph(Rectangle(30, 500, 100, 45));
+
+        icon.loadGraphics('#SunLogo', 0.15);
+        icon.toggleFisheye();    
+        world.addMorph(icon);
+    }
+
+    // example weather morph
+    var showWeather = !Config.skipMostExamples;
+    if (showWeather) {
+        // Maybe the icons should have rectangular images (unlike here)
+        new WeatherWidget().openIn(world, pt(700, 50));
+    }
+
+    var showTester = true;
+    if (showTester) new WidgetTester().openIn(world, pt(460, 20));
+
+    var showBrowser = !Config.skipMostExamples || Config.browserAnyway;
+    if (showBrowser) new SimpleBrowser().openIn(world, pt(20,20));
+
+    var showStocks = !Config.skipMostExamples;
+    if (showStocks) {
+        var stockWidget = new StockWidget();
+        stockWidget.startSteppingRefreshCharts(stockWidget.openIn(world, pt(300, 500)));
+    }
+
+    var showMessenger = !Config.skipMostExamples;
+    if (showMessenger) new MessengerWidget().openIn(world, pt(30, 600));
+
+    // TODO list morph
+    var showTODO = !Config.skipMostExamples;
+    if (showTODO) {
+        var todoMorph = TextMorph(Rectangle(440, 260, 250,20),
+                        "TODO:\n" + 
+                        "Browser specific configs(?)\n" +
+                        "Browser specific info can be alerted with command: Prototype.Browser.AlertBrowserInfo();\n" +
+                        "BuZilla up and running for the release\n" +
+                        "More widgets?\n" +
+                        "-Widget for managing timers\n" + 
+                        "-\"virtual desktop\" widget to drag-and-drop morph to another world?\n"
+                        );
+        todoMorph.shape.roundEdgesBy(10);
+        todoMorph.shape.setFillOpacity(.7);
+        world.addMorph(todoMorph);
+    }
+
     var innerWorld = !Config.skipMostExamples;
     if (innerWorld) {
-        world.addMorph(widget = LinkMorph(null, pt(260, 460)));
+        
+        var lm1 = LinkMorph(null, pt(280, 400));
+        world.addMorph(lm1);
 
-        widget.myWorld.onEnter = function() {
+        lm1.myWorld.onEnter = function() {
 
             var showCanvasScape = true;
             if (showCanvasScape) {
-                if (!widget.myWorld.csMorph) {
-                    console.log("creating csm");
+                if (!lm1.myWorld.csMorph) {
                     var csm = CanvasScapeMorph(Rectangle(30,30,800,300)/*pt(400, 350).extent(pt(800, 300))*/);
-                    widget.myWorld.csMorph = widget.myWorld.addMorph(WindowMorph(csm, 'CanvasScape'));
-                    //csm.startSteppingFunction(200, function(msTime) { this.changeKey( 37, 1); });
-                    console.log("creating csm done");
+                    lm1.myWorld.csMorph = lm1.myWorld.addMorph(WindowMorph(csm, 'CanvasScape'));
                 }
             }
 
             var showMap = true;
             if (showMap) {
-                if (!widget.myWorld.mapMorph) {
+                if (!lm1.myWorld.mapMorph) {
                     var tile = apps.maps.tileExtent;
                     var map = apps.maps.MapFrameMorph(new Rectangle(0, 0, 2*tile.x, 2*tile.y), true);
                     map.setScale(0.7);
                     map.setPosition(pt(320, 275));
-                    widget.myWorld.addMorph(map);
-                    widget.myWorld.mapMorph = map;
+                    lm1.myWorld.addMorph(map);
+                    lm1.myWorld.mapMorph = map;
                 }
             }
 
-            if (!widget.myWorld.rssReader) {
+            if (!lm1.myWorld.rssReader) {
                 console.log('initting RSS reader');
-                widget.myWorld.rssReader = new Feed("http://news.com.com/2547-1_3-0-5.xml").openIn(widget.myWorld, pt(725, 120));
+                lm1.myWorld.rssReader = new Feed("http://news.com.com/2547-1_3-0-5.xml").openIn(lm1.myWorld, pt(725, 120));
             }
 
         }
@@ -197,26 +178,26 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
             "Point=e&latitude_e6=61500000&longitude_e6=-3191200000&zm=8000&w=" +
             width + "&h=" + height + "&cc=US&min_priority=2";
             
-            widget.myWorld.addMorphBack(WindowMorph(ImageMorph(Rectangle(50, 10, width, height), url), 'Tampere'));
+            lm1.myWorld.addMorphBack(WindowMorph(ImageMorph(Rectangle(50, 10, width, height), url), 'Tampere'));
         }
 
         var showDoodle = true;
-        if (showDoodle) widget.myWorld.addMorph(WindowMorph(DoodleMorph(pt(875, 350).extent(pt(300, 300))), 
+        if (showDoodle) lm1.myWorld.addMorph(WindowMorph(DoodleMorph(pt(875, 350).extent(pt(300, 300))), 
                                                 'Doodle Morph'));
     }
     
     var slideWorld = true;
     if (slideWorld) { // Make a slide for "turning web programming upside down"
-        var lm = LinkMorph(null, pt(260, 520));
+        var lm2 = LinkMorph(null, pt(280, 460));
 
         // KP: note that element deletion interferes with iteration, so
         // we make an array first and then remove 
-        lm.myWorld.submorphs.toArray().each(function(m) { 
+        lm2.myWorld.submorphs.toArray().each(function(m) { 
             if (m instanceof LinkMorph) return;
             m.remove(); 
         });
         
-        // lm.setPosition(lm.position().addXY(65,0));
+        // lm2.setPosition(lm2.position().addXY(65,0));
         var loc = pt(100, 200);
         var captions = ["               JavaScript","            Widget World","     HTML, DOM, CSS, ETC...","                Browser","   bios:  Network,  Graphics"];
     
@@ -225,73 +206,74 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
             txt.setFontSize(20);
             txt.setFill(Color.hsb(70*i,0.7,0.8));
             loc = loc.addXY(0,33);
-            lm.myWorld.addMorph(txt); 
+            lm2.myWorld.addMorph(txt); 
         }
     
-        world.addMorph(lm); 
+        world.addMorph(lm2); 
+
+        var showSampleMorphs = true;
+        if (showSampleMorphs){
+            var colors = Color.wheel(4);
+            var loc = pt(30,450); 
+            var widgetExtent = pt(70, 30);
+            var dy = pt(0,50); 
+            var dx = pt(120,0);
+     
+            // Create a sample rectangle       
+            widget = Morph(loc.extent(widgetExtent), "rect");
+            widget.setFill(colors[0]);
+            lm2.myWorld.addMorph(widget);
+  
+            //widget = Morph.fromMarkup('<g type="Morph" transform="translate(210,395)"><ellipse cx="0" cy="0" rx="35" ry="15" type="ellicapse" fill="rgb(98,179,17)" stroke-width="1" stroke="rgb(0,0,0)"/><g type="Submorphs"/></g>');
+  
+            // Create a sample ellipse
+            widget = Morph(loc.addPt(dx).extent(widgetExtent), "ellipse");
+            widget.setFill(colors[1]);
+            lm2.myWorld.addMorph(widget);
+  
+            // Create a sample line
+            loc = loc.addPt(dy);
+            widget = Morph.makeLine([loc.addXY(0,15),loc.addXY(70,15)], 2, Color.black);
+            lm2.myWorld.addMorph(widget);
+      
+            // Create a sample polygon
+            var l2 = loc.addPt(dx);
+            widget = Morph(l2.asRectangle(),"rect");
+            widget.setShape(PolygonShape([pt(0,0),pt(70,0),pt(40,30),pt(0,0)], colors[2],1,Color.black));
+            lm2.myWorld.addMorph(widget);
+            loc = loc.addPt(dy);    
+      
+            // Create sample text widgets
+            var showBigText = true;
+            if (showBigText) {
+                widget = TextMorph(loc.extent(pt(100,50)),"Big Text"); // big text
+                widget.setFontSize(20);
+                widget.setTextColor(Color.blue);
+                lm2.myWorld.addMorph(widget);
+
+                widget = TextMorph(loc.addPt(dx).extent(pt(140,50)),"Unbordered"); // unbordered text
+                widget.setFontSize(20);  
+                widget.setBorderWidth(0);  
+                widget.setFill(null);
+                lm2.myWorld.addMorph(widget); 
+            }
+        }
+
     }
-
-    // TODO list morph
-    var showTODO = !Config.skipMostExamples;
-    if (showTODO) {
-        var todoMorph = TextMorph(Rectangle(450, 300, 250,20),
-                        "TODO:\n" + 
-                        "Browser specific configs(?)\n" +
-                        "Browser specific info can be alerted with command: Prototype.Browser.AlertBrowserInfo();\n" +
-                        "BuZilla up and running for the release\n" +
-                        "More widgets?\n" +
-                        "-Widget for managing timers\n" + 
-                        "-\"virtual desktop\" widget to drag-and-drop morph to another world?\n"
-                        );
-        todoMorph.shape.roundEdgesBy(10);
-        todoMorph.shape.setFillOpacity(.7);
-        world.addMorph(todoMorph);
-    }
-
-    // example icon morph with a fisheye effect 'on'
-    var showIcon = !Config.skipMostExamples;
-    if (showIcon) {
-        // maybe the icons should have a rectangle shaped images (unlike here)
-        //var icon = ImageMorph(Rectangle(30, 360, 80, 50), "http://logos.sun.com/images/SunSample.gif");
-        var icon = ImageMorph(Rectangle(30, 360, 100, 45));
-
-        icon.loadGraphics('#SunLogo', 0.15);
-        icon.toggleFisheye();    
-        world.addMorph(icon);
-    }
-
-    // example weather morph
-    var showWeather = !Config.skipMostExamples;
-    if (showWeather) {
-        // Maybe the icons should have rectangular images (unlike here)
-        new WeatherWidget().openIn(world, pt(700, 50));
-    }
-
-    var showWidgets = true;
-    if (showWidgets) new WidgetTester().openIn(world, pt(460, 20));
-
-    var showBrowser = !Config.skipMostExamples || Config.browserAnyway;
-    if (showBrowser) new SimpleBrowser().openIn(world, pt(20,20));
-
-    var showStocks = !Config.skipMostExamples;
-    if (showStocks) {
-        var stockWidget = new StockWidget();
-        stockWidget.startSteppingRefreshCharts(stockWidget.openIn(world, pt(300, 500)));
-    }
-
-    var showMessenger = !Config.skipMostExamples;
-    if (showMessenger) new MessengerWidget().openIn(world, pt(30, 600));
 
     return world;
 }
 
 function main() {
+    // Create an empty world
     var world = WorldMorph.createEmptyWorld();
     WorldMorph.setCurrent(world);
     world.displayWorldOn(Canvas);
     console.log('made world');
     if (Config.skipAllExamples) return;
-    WorldMorph.populateWithExamples(world, !Config.skipMostExamples);
+
+    // Populate the world with sample objects, widgets and applications
+    populateWorldWithExamples(world, !Config.skipMostExamples);
 }
 
 main();
