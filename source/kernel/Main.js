3c54d8dd8d22e0286caa6f47a3c5bdf6f1b3feb8
Antero Taivalsaari <antero.taivalsaari@nokia.com>
2007-09-05 07:45:05 +0000

    Restructuring/cleaning up the current demo world.

--- source/kernel/Main.js
+++ source/kernel/Main.js
@@ -6,7 +6,6 @@ var Global = this;
 
 Config.shiftDragForDup = true; // easy duplicate gesture
 
-
 WorldMorph.populateWithExamples = function(world, otherWorld, server) {
 
     //var widget; 
@@ -14,79 +13,79 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
     var widget, zzRect, zzEll, zzLine, zzPoly, zzText, zzScript; // Comment to make these global for debugging access
     var showSampleMorphs = true;
     if (showSampleMorphs){
-      var colors = Color.wheel(4);
-      var loc = pt(30,450); 
-      var widgetExtent = pt(70, 30);
-      var dy = pt(0,50); 
-      var dx = pt(120,0);
-      // Create a sample rectangle       
-      widget = Morph(loc.extent(widgetExtent), "rect");
+        var colors = Color.wheel(4);
+        var loc = pt(30,450); 
+        var widgetExtent = pt(70, 30);
+        var dy = pt(0,50); 
+        var dx = pt(120,0);
+     
+        // Create a sample rectangle       
+        widget = Morph(loc.extent(widgetExtent), "rect");
+        widget.setFill(colors[0]);
+        world.addMorph(widget);
+        zzRect = widget;
   
-      widget.setFill(colors[0]);
-      world.addMorph(widget);
-      zzRect = widget;
+        //widget = Morph.fromMarkup('<g type="Morph" transform="translate(210,395)"><ellipse cx="0" cy="0" rx="35" ry="15" type="ellicapse" fill="rgb(98,179,17)" stroke-width="1" stroke="rgb(0,0,0)"/><g type="Submorphs"/></g>');
   
-      //widget = Morph.fromMarkup('<g type="Morph" transform="translate(210,395)"><ellipse cx="0" cy="0" rx="35" ry="15" type="ellicapse" fill="rgb(98,179,17)" stroke-width="1" stroke="rgb(0,0,0)"/><g type="Submorphs"/></g>');
+        // Create a sample ellipse
+        widget = Morph(loc.addPt(dx).extent(widgetExtent), "ellipse");
+        widget.setFill(colors[1]);
+        world.addMorph(widget);
+        zzEll = widget;
   
-      // Create a sample ellipse
-      widget = Morph(loc.addPt(dx).extent(widgetExtent), "ellipse");
-      widget.setFill(colors[1]);
-  
-      world.addMorph(widget);
-      zzEll = widget;
-  
-      // Create a sample line
-      loc = loc.addPt(dy);
-      widget = Morph.makeLine([loc.addXY(0,15),loc.addXY(70,15)], 2, Color.black);
-      world.addMorph(widget);
-      zzLine = widget;
-      var l2 = loc.addPt(dx);
+        // Create a sample line
+        loc = loc.addPt(dy);
+        widget = Morph.makeLine([loc.addXY(0,15),loc.addXY(70,15)], 2, Color.black);
+        world.addMorph(widget);
+        zzLine = widget;
       
-      // Create a sample polygon
-      widget = Morph(l2.asRectangle(),"rect");
-      widget.setShape(PolygonShape([pt(0,0),pt(70,0),pt(40,30),pt(0,0)], colors[2],1,Color.black));
-      world.addMorph(widget);
-      zzPoly = widget;
-      loc = loc.addPt(dy);    
+        // Create a sample polygon
+        var l2 = loc.addPt(dx);
+        widget = Morph(l2.asRectangle(),"rect");
+        widget.setShape(PolygonShape([pt(0,0),pt(70,0),pt(40,30),pt(0,0)], colors[2],1,Color.black));
+        world.addMorph(widget);
+        zzPoly = widget;
+        loc = loc.addPt(dy);    
       
-      // Create sample text widgets
-      var showBigText = true;
-      if (showBigText) {
-          widget = TextMorph(loc.extent(pt(100,50)),"Big Text"); // big text
-          widget.setFontSize(20);
-          widget.setTextColor(Color.blue);
-          world.addMorph(widget);
-          zzText = widget;
-          widget = TextMorph(loc.addPt(dx).extent(pt(140,50)),"Unbordered"); // unbordered text
-          widget.setFontSize(20);  
-          widget.setBorderWidth(0);  
-          widget.setFill(null);
-          world.addMorph(widget); 
-      }
+        // Create sample text widgets
+        var showBigText = true;
+        if (showBigText) {
+            widget = TextMorph(loc.extent(pt(100,50)),"Big Text"); // big text
+            widget.setFontSize(20);
+            widget.setTextColor(Color.blue);
+            world.addMorph(widget);
+            zzText = widget;
+            widget = TextMorph(loc.addPt(dx).extent(pt(140,50)),"Unbordered"); // unbordered text
+            widget.setFontSize(20);  
+            widget.setBorderWidth(0);  
+            widget.setFill(null);
+            world.addMorph(widget); 
+        }
           
-      // Sample executable script pane
-      var showPenScript = true;
-      if (showPenScript) {
-          widget = TextMorph(pt(50,30).extent(pt(250,50)),Pen.script);
-          widget.align(widget.bounds().bottomRight(), world.bounds().topRight().addPt(pt(-50,100))); 
-          world.addMorph(widget);
-          zzScript = widget; 
-      }
-  
-      if (Config.showTestText) {
-          Pen.script = "gggggggggg " +
-          "hhhhhhhhhh\n" +
-          "iiiiiiiiii " +
-          "jjjjjjjjjj\n" +
-          "kkkkkkkkkk " +
-          "llllllllll\n" +
-          "mmmmmmmmmm " +
-          "nnnnnnnnnn";
-          widget = TestTextMorph(pt(50,50).extent(pt(250,50)), Pen.script);
-          world.addMorph(widget);
-          widget.setScale(3.0);
-      }
+        // Sample executable script pane
+        var showPenScript = true;
+        if (showPenScript) {
+            widget = TextMorph(pt(50,30).extent(pt(250,50)),Pen.script);
+            widget.align(widget.bounds().bottomRight(), world.bounds().topRight().addPt(pt(-50,100))); 
+            world.addMorph(widget);
+            zzScript = widget; 
+        }
+
+        if (Config.showTestText) {
+            Pen.script = "gggggggggg " +
+            "hhhhhhhhhh\n" +
+            "iiiiiiiiii " +
+            "jjjjjjjjjj\n" +
+            "kkkkkkkkkk " +
+            "llllllllll\n" +
+            "mmmmmmmmmm " +
+            "nnnnnnnnnn";
+            widget = TestTextMorph(pt(50,50).extent(pt(250,50)), Pen.script);
+            world.addMorph(widget);
+            widget.setScale(3.0);
+        }
     }
+   
     // zzHand = world.worldState.hands[0];
     var showStar = true;
     if (showStar) {  // Make a star
@@ -110,22 +109,26 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
         world.addMorph(widget);
             
         var spinningStar = !Config.skipMostExamples || Config.spinningStar;
-	if(spinningStar) {  // Make the star spin as a test of stepping	    if (Config.useNewScheduler) {
-		widget.startStepping(1/20, "rotateBy", 0.1);
-	    } else {
-		widget.startSteppingFunction(60,function(msTime) {			this.setRotation(this.getRotation() + 0.1); }) }
-	}    }
+        if(spinningStar) {  // Make the star spin as a test of stepping
+            if (Config.useNewScheduler) {
+                widget.startStepping(1/20, "rotateBy", 0.1);
+            } else {
+                widget.startSteppingFunction(60,function(msTime) {
+                this.setRotation(this.getRotation() + 0.1); }) 
+            }
+        }
+    }
 
     var showClock = true;
     if (showClock) {
         widget = ClockMorph(pt(500, 525), 50);
         // clockWidget.addClipRect(Rectangle(20,20,80,80));
         world.addMorph(widget);
-	if (Config.useNewScheduler) {
-		widget.startSteppingScripts(); // not yet
-	} else {
-        	widget.startStepping(1000);
-	}
+        if (Config.useNewScheduler) {
+            widget.startSteppingScripts(); // not yet
+        } else {
+            widget.startStepping(1000);
+        }
     }
 
     var hilbertFun = true;
@@ -154,9 +157,15 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
 
         widget.myWorld.onEnter = function() {
 
-            if (!widget.myWorld.rssReader) {
-                console.log('initting RSS reader');
-                widget.myWorld.rssReader = new Feed("http://news.com.com/2547-1_3-0-5.xml").openIn(widget.myWorld, pt(725, 120));
+            var showCanvasScape = true;
+            if (showCanvasScape) {
+                if (!widget.myWorld.csMorph) {
+                    console.log("creating csm");
+                    var csm = CanvasScapeMorph(Rectangle(30,30,800,300)/*pt(400, 350).extent(pt(800, 300))*/);
+                    widget.myWorld.csMorph = widget.myWorld.addMorph(WindowMorph(csm, 'CanvasScape'));
+                    //csm.startSteppingFunction(200, function(msTime) { this.changeKey( 37, 1); });
+                    console.log("creating csm done");
+                }
             }
 
             var showMap = true;
@@ -165,21 +174,17 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
                     var tile = apps.maps.tileExtent;
                     var map = apps.maps.MapFrameMorph(new Rectangle(0, 0, 2*tile.x, 2*tile.y), true);
                     map.setScale(0.7);
-                    map.setPosition(pt(320, 175));
+                    map.setPosition(pt(320, 275));
                     widget.myWorld.addMorph(map);
                     widget.myWorld.mapMorph = map;
                 }
             }
-            var showCanvasScape = true;
-            if (showCanvasScape) {
-                if (!widget.myWorld.csMorph) {
-                    console.log("creating csm");
-                    var csm = CanvasScapeMorph(Rectangle(20,400,800,300)/*pt(400, 350).extent(pt(800, 300))*/);
-                    widget.myWorld.csMorph = widget.myWorld.addMorph(WindowMorph(csm, 'CanvasScape'));
-                    //csm.startSteppingFunction(200, function(msTime) { this.changeKey( 37, 1); });
-                    console.log("creating csm done");
-                }
+
+            if (!widget.myWorld.rssReader) {
+                console.log('initting RSS reader');
+                widget.myWorld.rssReader = new Feed("http://news.com.com/2547-1_3-0-5.xml").openIn(widget.myWorld, pt(725, 120));
             }
+
         }
 
         var showBitmap = false;
@@ -276,8 +281,6 @@ WorldMorph.populateWithExamples = function(world, otherWorld, server) {
 
     var showMessenger = !Config.skipMostExamples;
     if (showMessenger) new MessengerWidget().openIn(world, pt(30, 600));
-  
-    
 
     return world;
 }
