6af226b506ecc16c9ac90228a2c3315f96106563
Antero Taivalsaari <antero.taivalsaari@nokia.com>
2007-09-19 19:22:10 +0000

    More tutorial improvements.  Almost done now.

---
+++ source/tutorial/Tutorial16-Misc1.js
@@ -0,0 +1,50 @@
+/**
+ * @class TutorialMorph
+ */
+
+TutorialMorph = HostClass.create('TutorialMorph', Morph);
+
+// Make sure the application knows about the Global context
+Global = this;
+
+Object.extend(TutorialMorph.prototype, {
+
+    // Reference to the world 
+    world: null,
+
+    // Function to populate the world
+    populateWorld: function(world) { 
+
+        // Open a class browser    
+        var widget = new SimpleBrowser().openIn(world, pt(20,20));
+
+    },
+
+    // Function that creates the tutorial demo
+    createTutorial: function() {
+        
+        // Create an empty world
+        world = WorldMorph.createEmptyWorld();
+        
+        // Set the world current and display it
+        WorldMorph.setCurrent(world);
+        world.displayWorldOn(Canvas);
+
+        // Populate the world with sample objects
+        this.populateWorld(world);        
+    },
+    
+    // Demo instantiation
+    initialize: function() {
+        this.createTutorial();
+    }
+
+});
+
+// Instantiate the tutorial
+function main() {
+    TutorialMorph();
+}
+
+main();
+
