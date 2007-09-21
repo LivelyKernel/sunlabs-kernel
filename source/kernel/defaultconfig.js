1b4c2243884fbaa5ded0dbf53f7d09acc12b6af7
Antero Taivalsaari <antero.taivalsaari@nokia.com>
2007-09-21 11:27:42 +0000

    Added system-level configuration items from Main.js

--- source/kernel/defaultconfig.js
+++ source/kernel/defaultconfig.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2006-2007 Sun Microsystems, Inc.
+ * All rights reserved.  Use is subject to license terms.
+ * This distribution may include materials developed by third parties.
+ *  
+ * Sun, Sun Microsystems, the Sun logo, Java and JavaScript are trademarks
+ * or registered trademarks of Sun Microsystems, Inc. in the U.S. and
+ * other countries.
+ */ 
+
+/**
+ * defaultconfig.js.  System default configuration.
+ */
+
+Config.shiftDragForDup = true; // allows easy object duplication using the Shift key
+Config.useNewScheduler = true; // both schedulers active now
+
 Config.showWebStore = true;
 Config.skipMostExamples = false;
 Config.showThumbnail = false;
