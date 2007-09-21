b6b839f7de7384186bdfc0f5cb2b865b8c18c981
Antero Taivalsaari <antero.taivalsaari@nokia.com>
2007-09-21 11:20:54 +0000

    File for storing global default configurations of the system.

---
+++ source/kernel/defaultconfig.js
@@ -0,0 +1,7 @@
+Config.showWebStore = true;
+Config.skipMostExamples = false;
+Config.showThumbnail = false;
+Config.useNewScheduler = true;
+Config.loadFromMarkup = false;
+Config.skipAllExamples = Config.skipMostExamples && false;
+//Config.proxyURL = 'http://www.hanaalliance.org/jsl/proxy/';
