e09a29b87330316e134ea68fd3b582d21da33607
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2007-10-04 00:37:47 +0000

    Small fixes for proxying to a server on a different port

--- source/kernel/Network.js
+++ source/kernel/Network.js
@@ -49,14 +49,13 @@ Object.extend(NetRequest.prototype, {
             var splitter = new RegExp("http://([^/:]*)(:[0-9]+)?(/.*)");
             var urlMatch = url.match(splitter);
             var proxyMatch = Config.proxyURL.match(splitter);
-	    //console.warn("url match " + urlMatch + " on " + url);
-            if (urlMatch && proxyMatch && proxyMatch[1] != urlMatch[1]) {
-		if (urlMatch[2] != null) {
-		    console.warn("proxying on nondefault ports not supported: " + url);
-		} else {
-                    var result = Config.proxyURL + urlMatch[1]  + urlMatch[3];
-                    return result;
-		}
+	    var portMatch = urlMatch[2];
+	    if (portMatch) portMatch = "/" + portMatch.substring(1);  // replace ":" with "
+	    else portMatch = "";
+            if (urlMatch && proxyMatch && (proxyMatch[1] != urlMatch[1] || proxyMatch[2] != urlMatch[2])) {
+                var result = Config.proxyURL + urlMatch[1]  + portMatch + urlMatch[3];
+	    	//console.warn("url match " + urlMatch + " on " + url + " to " + result);
+                return result;
             }
         } 
         return url;
