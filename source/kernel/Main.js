a6ac8c5dab9dfeec2fcff7bf8fa8958ae53ee51d
Dan Ingalls <danhhingalls@gmail.com>
2008-04-01 21:56:03 +0000

    DI: Rich text stuff

--- source/kernel/Main.js
+++ source/kernel/Main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2006-2008 Sun Microsystems, Inc.
+ * Copyright ï¿½ 2006-2008 Sun Microsystems, Inc.
  * All rights reserved.  Use is subject to license terms.
  * This distribution may include materials developed by third parties.
  *  
@@ -315,11 +315,12 @@ function populateWorldWithExamples(world) {
 
         // Sample executable script pane
         if (Config.showPenScript) {
-            if (Config.showTestText) widget = new TestTextMorph(pt(50,30).extent(pt(250,50)), Pen.script);
+             if (Config.showTestText) widget = new TestTextMorph(pt(50,30).extent(pt(250,50)), Pen.script);
             else widget = new TextMorph(pt(50,30).extent(pt(250,50)), Pen.script);
             widget.align(widget.bounds().bottomRight(), world.bounds().topRight().addPt(pt(-150,100))); 
             devWorld.myWorld.addMorph(widget);
-        }
+        }		if (Config.showRichText) {			var txt = new String("But wait, there's more...");			txt.emphasize( {style: 'bold'}, 5, 8);			txt.emphasize( {size: 14, style: 'bold'}, 19, 22);			console.log(txt.style);			// widget = new TextMorph(widget.bounds().bottomLeft().addXY(0, 50).extent(pt(250, 50)), txt);
+            // devWorld.myWorld.addMorph(widget);		}
 
         if (Config.showHilbertFun) Pen.hilbertFun(devWorld.myWorld, widget.bounds().bottomLeft().addXY(180,80));
 
