bf58c2554ab0486e334c6ce5da3fedb85abe48a5
Stephen Uhler <stephen.uhler@sun.com>
2008-04-05 23:08:17 +0000

    Add analog dial VOIP phone demo.  set:   Config.showPhoneWorld do display.

--- source/kernel/Main.js
+++ source/kernel/Main.js
@@ -343,6 +343,17 @@ function populateWorldWithExamples(world) {
 
     }
 
+    if (Config.showPhoneWorld) {
+		var phoneWorld = new LinkMorph(null, pt(60, 320));
+		world.addMorph(phoneWorld);
+		var widgetTextMorph = 
+			new TextMorph(new Rectangle(90, 300, 100, 25), "Telephone Demo");
+		widgetTextMorph.shape.roundEdgesBy(10);
+		world.addMorph(widgetTextMorph);
+		phoneWorld.myWorld.onEnter = function() {
+			phoneDemo(phoneWorld.myWorld, pt(250,180), 150);
+		}
+    }
 
     if (Config.showXenoMorph) {
         XenoMorph.prototype.test(); //"http://livelykernel.sunlabs.com/index.html");//"http://www.opera.com");
