7835e4564af718b03694c9ebc2565df8fd386505
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2008-04-11 22:53:26 +0000

    Somehow recent changes to input line history management vanished, brought them back in.

--- source/kernel/Text.js
+++ source/kernel/Text.js
@@ -1023,22 +1023,49 @@ Morph.subclass("TextMorph", {
 	return this;
     },
 
-
-    beInputLine: function() {
-        //this.setWrapStyle(WrapStyle.None);
+    nextHistoryEntry: function() {
+	var history = this.getModelValue("getHistory");
+	if (!history || history.length == 0) return "";
+	var current = this.getModelValue("getHistoryCursor");
+	current = (current + 1) % history.length;
+	this.setModelValue("setHistoryCursor", current);
+	return history[current];
+    },
+    
+    previousHistoryEntry: function() {
+	var history = this.getModelValue("getHistory");
+	if (!history || history.length == 0) return "";
+	var current = this.getModelValue("getHistoryCursor");
+	current = (current + history.length - 1) % history.length;
+	this.setModelValue("setHistoryCursor", current);
+	return history[current];
+    },
+    
+    saveHistoryEntry: function(text, historySize) {
+	if (!historySize || !text) return;
+	var history = this.getModelValue("getHistory");
+	if (!history) history = [];
+	history.push(text);
+	history.length > historySize && history.unshift();
+	this.setModelValue("setHistory", history);
+	this.setModelValue("setHistoryCursor", history.length);
+    },
+    
+    beInputLine: function(historySize) {
         this.onKeyDown = function(evt) {
 	    switch (evt.getKeyCode()) {
 	    case Event.KEY_DOWN: 
-		this.setTextString(this.getModelValue("getNextHistoryEntry", ""));
+		historySize && this.setTextString(this.nextHistoryEntry());
 		this.setNullSelectionAt(this.textString.length);
 		evt.stop();
 		return true;
-	    case Event.KEY_UP: 
-		this.setTextString(this.getModelValue("getPreviousHistoryEntry", ""));
+	    case Event.KEY_UP:
+		historySize && this.setTextString(this.previousHistoryEntry());
 		this.setNullSelectionAt(this.textString.length);
 		evt.stop();
 		return true;
 	    case Event.KEY_RETURN:
+		historySize && this.saveHistoryEntry(this.textString, historySize);
 		this.saveContents(this.textString);
 		evt.stop();
 		return true;
