bbb308c5ba3f746e7faa91a6dcf36cf2959e5dfe
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2008-07-23 17:59:37 +0000

    added a minimal brazil build with a small fix, allows for multiple invocation of js from within the server

--- source/server/runbrazil.sh
+++ source/server/runbrazil.sh
@@ -4,11 +4,13 @@ project_root=../../..
 #js=../../lib/js.jar
 #js=${project_root}/trunk/lib/js.jar
 js=trunk/lib/js.jar
+
 #brazil=../../lib/brazil-04-Mar-08.jar
 #brazil=${project_root}/trunk/lib/brazil-04-Mar-08.jar
-brazil=trunk/lib/brazil-02-Jul-08.jar
+#brazil=trunk/lib/brazil-02-Jul-08.jar
+brazil=trunk/lib/brazil-minimal.jar
 
 cd $project_root
-echo pwd is `pwd`
+#echo pwd is `pwd`
 #exec java -classpath ${js}:${brazil} sunlabs.brazil.server.Main  -c $project_root/trunk/source/server/brazil.config
 exec java -classpath ${js}:${brazil} sunlabs.brazil.server.Main  -c trunk/source/server/brazil.config
