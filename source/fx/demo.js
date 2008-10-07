dee9dfc05107b4fd6cd68f5deb4855d014439025
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2008-10-07 20:24:44 +0000

    added copyright notice

--- source/fx/demo.js
+++ source/fx/demo.js
@@ -1,5 +1,14 @@
+/*
+ * Copyright � 2006-2008 Sun Microsystems, Inc.
+ * All rights reserved.  Use is subject to license terms.
+ * This distribution may include materials developed by third parties.
+ *  
+ * Sun, Sun Microsystems, the Sun logo, Java and JavaScript are trademarks
+ * or registered trademarks of Sun Microsystems, Inc. in the U.S. and
+ * other countries.
+ */ 
 
-var window = {}
+
 var fx = {
     Panel: Packages.com.sun.scenario.scenegraph.JSGPanel,
     Group: Packages.com.sun.scenario.scenegraph.SGGroup,
