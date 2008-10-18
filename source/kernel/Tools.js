73286aeb210b0e4810312d203db20a011ec577f7
Robert Krahn <robert.krahn@gmail.com>
2008-10-18 01:28:40 +0000

    SimpleBrowser supports highlighting optionally

--- source/kernel/Tools.js
+++ source/kernel/Tools.js
@@ -51,7 +51,16 @@ Widget.subclass('SimpleBrowser', {
         case p.getMethodName:
             var methodName = this.getModelValue("getMethodName");
             var className = this.getModelValue("getClassName");
-            this.setModelValue("setMethodString", this.getMethodStringFor(className, methodName));
+            var source = this.getMethodStringFor(className, methodName);    
+            if (Config.highlightSyntax)
+                require('Ometa.js').toRun(function() {
+                    source = new SyntaxHighlighter().highlightFunction(source);
+                    //FIXME for setting rich text, otherwise hard to implmenent currently
+                    var textMorph = this.getModel().dependents.detect(function(ea) { return ea instanceof TextMorph});
+                    textMorph.setRichText(source);
+                }.bind(this));
+            else this.setModelValue("setMethodString", source);
+            
             break;
         case p.getMethodString:
             var className = this.getModelValue("getClassName");
@@ -93,13 +102,14 @@ Widget.subclass('SimpleBrowser', {
     getMethodStringFor: function(className, methodName) { 
         if (!className || !methodName) return "no code"; 
 	var source = null;
-	if (module.SourceControl != null) 
+	if (module.SourceControl) 
 	    source = module.SourceControl.getSourceInClassForMethod(className, methodName);
-	if (source) return source;
-	var func = (className == "Global") ? Global[methodName] : Global[className].prototype[methodName];
-	if (func == null) return "no code";
-	var code = func.getOriginal().toString();
-	return code;
+	else {
+	    var func = (className == "Global") ? Global[methodName] : Global[className].prototype[methodName];
+	    if (!func) return "no code";
+	    source = func.getOriginal().toString();
+	}
+	return source;
     },
     
     buildView: function(extent) {
