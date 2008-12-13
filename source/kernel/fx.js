d7eb3ea23aa96b283532d86d0f7fcea1099d8e03
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2008-12-13 03:19:22 +0000

    beginnings of a new standard library, unused yet

---
+++ source/kernel/fx.js
@@ -0,0 +1,459 @@
+/*
+ * Copyright ï¿½ 2006-2008 Sun Microsystems, Inc.
+ * All rights reserved.  Use is subject to license terms.
+ * This distribution may include materials developed by third parties.
+ *  
+ * Sun, Sun Microsystems, the Sun logo, Java and JavaScript are trademarks
+ * or registered trademarks of Sun Microsystems, Inc. in the U.S. and
+ * other countries.
+ */ 
+ 
+    
+if (this.arguments && (arguments[0] === 'script')) {
+    // if called from command line with argument 'script', we'll initialize what is needed for rhino.
+    // can be run as $ jrunscript fx.js script
+    var window = this;
+    load('rhino-compat.js');
+    load('JSON.js');
+    load('miniprototype.js');
+    load('Base.js');
+}
+
+// Ideally this would be the single top level variable introduced into the system.
+// the name fx is short and generic. No reference to existing products necessarily implied.
+// Should not make assumptions about other libraries, besides standard JS libraries (incl ES3.1)
+// Could act as a capability, i.e, if the fx object is not available, various runtime things can't happen
+var fx = (function() {
+    function Aux(args) {
+	this.content = Array.prototype.concat.apply([], args);
+    }
+
+    Object.defineProperties(Aux.prototype, {
+	run: {
+	    // convenience to do 'fx().run(function() { })
+	    value: function(inner) {
+		var args = this.content;
+		return inner.apply(args[0], args); 
+	    }
+	}
+	
+    });
+    return function fx() {
+	return new Aux(arguments);
+    }
+})();
+
+
+fx().run(function() { // scope function
+
+    Object.defineProperties(fx, {
+	
+	extend: { 
+	    documentation: "creates a new class",
+	    value: function(base, derived) {
+		derived = derived || Functions.K;
+		
+		function constr(/*...*/) {
+		    // this is initialized to a new object
+		    Array.prototype.unshift.call(arguments, base.bind(this));
+		    derived.apply(this, arguments);
+		    return this;
+		}
+		constr.prototype = Object.create(base.prototype);
+		constr.prototype.constructor = constr;
+		constr.extend = Object.freeze(function(derived) {
+		    // convenience method, should be lifted
+		    // if derived not specified, lets just call the argument, which should be the superconstructor,
+		    // with no arguments
+		    derived = derived || function(superconstructor) { superconstructor()}; 
+		    return fx.extend(this, derived);
+		});
+		
+		return constr;
+	    }
+	},
+	
+	defineSlot: { 
+	    documentation: "new member (re)definition pattern, inspired by ES3.1 Object.defineProperty",
+	    value: function(target, name, descriptor) {
+		
+		var previousValue = target[name];
+		if (previousValue !== undefined) {
+		    if (!target.hasOwnProperty(name)) {
+			if (descriptor.override == false) {
+			    // name is inherited and not defined locally, we want to shadow it but don't declare it
+			    throw new Error('undeclared override of ' + name);
+			} else if (descriptor.value instanceof Function) { // note that Safari regexps may be functions ..
+			    if(!(previousValue instanceof Function)) 
+				throw new Error('overriding non-function ' + name + ' with a function');
+			    var defined = descriptor.value;
+			    // FIXME: alloc new descriptor instead of modifying the old one?
+			    descriptor.value = function wrapper() {
+				Array.prototype.unshift.call(arguments, previousValue.bind(this));
+				return defined.apply(this, arguments);
+			    }
+			}
+		    } // otherwise we're overwriting own property, not present up the proto chain
+		    
+		} else {
+		    // property is not defined anywhere
+		    if (descriptor.override == true) {
+			throw new Error('property ' + name + ' declared as override but nothing to override');
+		    }
+		}
+		
+		// FIXME: only if fx.Object is 
+		if (!target.hasOwnProperty('$schema')) { 
+		    // if we inherit a schema, derive from it, otherwise create a regular object.
+		    var schema = Object.create(target.$schema || Object.prototype);
+		    schema[name] = descriptor;
+		    
+		    Object.defineProperty(target, "$schema", { enumerable: false, value: schema});
+		}  else target.$schema[name] = descriptor;
+		
+		return Object.defineProperty(target, name, descriptor);
+		// FIXME: react to 'descriptor.observable', send updates ?
+		// react to descriptor.replace, allow local replace of previously defined function?
+	    }
+	    
+	},
+	
+	defineSlots: { 
+	    documentation: "new member (re)definition pattern, inspired by ES3.1 Object.defineProperties",
+	    value: function(target, descriptorSet) {
+		for (var name in descriptorSet) {
+		    if (descriptorSet.hasOwnProperty(name))
+			fx.defineSlot(target, name, descriptorSet[name]);
+		}
+		return target;
+	    }
+	},
+	
+	clone: { 
+	    documentation: "invoke the optional filter on every source member before assigning to target",
+	    value: function(source, filter) { 
+		var dest = Object.create(Object.getPrototypeOf(source));
+		for (var name in source) 
+		    if (source.hasOwnProperty(name)) {
+			// doesnt handle getters and setters
+			dest[name] = filter(source, name);
+		    }
+		return dest;
+	    }
+	},
+	
+	shallowCopy: {
+	    description: "shallow clone",
+	    value: function(source, optBlacklist) {
+		function filter(source, name) {
+		    if (optBlacklist && optBlacklist.indexOf(name) >= 0) return undefined;
+		    else return source[name];
+		}
+		return fx.clone(source, filter);
+	    }
+	},
+	
+	deepCopy: { 
+	    documentation: ["clone using a default deep cloner",
+			    "not terribly efficient due to apparent lack of hashcodes in javascript"],
+	    value: function(source, optBlacklist) {
+		// consider fast path for objects that have hashcodes
+		var visited = []; // visited.lastIndexOf(object) is the index of objects' copy in the copies array
+		var copies = []; 
+		function cloner(source, name) {
+		    if (optBlacklist && optBlacklist.indexOf(name) >= 0) return undefined;
+		    var object = source[name];
+		    if (typeof object === 'object') {
+			var index = visited.lastIndexOf(object); // ouch, array positions as hash codes
+			if (index < 0) {
+			    visited.push(object);
+			    var copy = fx.clone(object, cloner);
+			    copies.push(copy);
+			    return copy;
+			}  else return copies[index];
+		    } else return object; // primitive
+		}
+		return fx.clone(source, cloner);
+	    }
+	},
+	
+	visitProperties: { 
+	    documentation: "visit all the own properties",
+	    value: function(object, visitor) { 
+		for (var name in object) 
+		    if (object.hasOwnProperty(name)) {
+			// doesnt handle getters and setters
+			visitor(object, name);
+		    }
+		return object;
+	    }
+	},
+	
+	
+	visitAll: {
+	    value: function(source, inspector) {
+		// consider fast path for objects that have hashcodes
+		var visited = []; 
+		inspector = inspector || Functions.Empty;
+		function visitor(object, name) {
+		    var value = object[name];
+		    // value is the value of the property
+		    //var value = dereferencer(object, name); // pre-order visit
+		    if (typeof value === 'object') {
+			var index = visited.lastIndexOf(value); // ouch, array positions as hash codes
+			if (index < 0) {
+			    //print('visiting ' + value + ' with new id ' + visited.length);
+			    index = visited.length;
+			    visited.push(value);
+			    inspector(object, name, index);
+			    fx.visitProperties(value, visitor);
+			} else {
+			    inspector(object, name, index);
+			    //print('not recursing into ' + value + ' with id ' + index);
+			}
+		    }  else {
+			inspector(object, name, undefined);
+		    }
+		}
+		return fx.visitProperties(source, visitor);
+	    }
+	}
+    });
+    
+    Object.defineProperties(fx, {
+	Object: { // convenience base class
+	    value: fx.extend(Object)
+	}
+    });
+    
+    
+    fx.defineSlots(fx.Object.prototype, {
+	defineSlots: {
+	    description: "convenience method, redirects to fx.defineSlots",
+	    value: function(set) {
+		return fx.defineSlots(this, set);
+	    }
+	},
+	
+	defineSlot: {
+	    description: "convenience method, redirects to fx.defineSlot",
+	    value: function(name, descriptor) {
+		return fx.defineSlot(this, name, descriptor);
+	    }
+	}
+	
+    });
+});
+    
+if (this.arguments && (arguments[1] === 'test')) {
+    fx().run(function() {
+	function test1() {
+	    var TestObj = fx.Object.extend();
+	    
+	    TestObj.prototype.defineSlots({
+		yo: {value: function() { print('yo') }}
+	    });
+	    // do we want TextObj.prototype.defineSlots() as well?
+	    
+	    
+	    var t = new TestObj();
+	    t.yo();
+	    print('check ' + [t instanceof TestObj]);
+	    
+	    var Test2 = TestObj.extend();
+	    
+	    var t = new Test2();
+	    t.yo();
+	    print('check ' + [t instanceof Test2, t instanceof TestObj]);
+	    
+	    var OriginalPerson = fx.Object.extend();
+	    
+	    OriginalPerson.prototype.defineSlots({
+		sayHello: { value: function() {
+		    return "Hello, my name is " + this.getName();
+		}},
+		getName: { value: function() {return 'Adam';}},
+		tag: { value: 'OriginalPerson', override: false}
+	    });
+	    
+	    var Person = OriginalPerson.extend(function(inherited, name) {
+		inherited();
+		this.getName = function() {return name || 'Anonymous';};
+	    });
+	    
+	    Person.prototype.defineSlot('tag', { value: 'Person'});
+	    Person.prototype.defineSlot('toString', { value: function() { return "aPerson"}});
+	    
+	    
+	    var p = new Person('myself');
+	    p.defineSlot('tag', {value: 'special person'});
+	    print('checking p ' + [p instanceof Person, p instanceof OriginalPerson]);
+
+	    
+	    var p2 = Object.create(p);
+	    p2.defineSlot('tag', {value: 'derived'});
+	    print('derived is ' + p2.tag + ', ' + p2.constructor.name);
+	    
+	    
+	    var Guru = Person.extend(function(inherited, name, topic) {
+		inherited(name);
+		this.getTopic = function() {return topic || 'none';}
+	    });
+	    //    print('guru prototype name ' + Guru.prototype.getName());
+	    var God = Guru.extend(function(inherited) {
+		inherited('Almighty', 'All');
+	    });
+	    
+	    Person.prototype.defineSlot("yo", { value: function() { return "yo" } });
+	    
+	    var h =  new Guru('Douglas Crockford', 'JavaScript');
+	    var h2 =  new Guru('Yoda', 'Something Star Wars');
+	    
+	    
+	    print('proto sharing? ' + (Object.getPrototypeOf(h) === Object.getPrototypeOf(h2)));
+	    print('checking h ' + [h instanceof Guru, h instanceof Person, h instanceof OriginalPerson]);
+	    
+	    print(new God() instanceof Person);
+	    print('is persons prototype a person? ' + (Object.getPrototypeOf(new Person({name: 'Fred'})) instanceof Person));
+	    
+	    print('is Gurus prototype a Person? ' + (Object.getPrototypeOf(h) instanceof Person));
+	    
+	    print('is Gurus prototype a Guru? ' + (Object.getPrototypeOf(h) instanceof Guru));
+	    print('is this Gurus prototype Guru.prototype? ' + (Object.getPrototypeOf(h) === Guru.prototype));
+	    
+	    function protochain(target, optAcc) {
+		var acc = optAcc || [];
+		var proto = Object.getPrototypeOf(target);
+		if (proto) {
+		    acc.push(proto);
+		    protochain(proto, acc);
+		}
+		return acc;
+	    }
+	    
+	    print(h.yo());
+	    Person.prototype.defineSlot("yo", { // no override 
+		value: function() { return 'whassup' }
+	    });
+	    print(h.yo());
+
+	    print([h.getName(), h.getTopic()]);    
+	    print([h2.getName(), h2.getTopic()]);    
+	    
+	    //Object.prototype.toString = function() { return this.tag};
+	    print('gods chain is ' + protochain(new God()));
+	    print(h.tag + ' gurus chain is ' + protochain(h));
+	    print(h2.tag + ' gurus chain is ' + protochain(h));
+	    
+	    
+	    Guru.prototype.defineSlot("yo", {
+		value: function(inherited, arg) { return inherited() + "!!!" + (arg || "") }, override: true
+	    });
+	    print('God says ' + new God().yo(', yo'));
+	    
+	}
+	
+	test1();
+	
+	function test2() {
+	    
+	    var Morph = fx.Object.extend(function(inherited, shape) {
+		this.shape = shape;
+	    });
+	    
+	    Morph.prototype.defineSlot("getShape", { value: function() { return this.shape;}});
+	    
+	    var BoxMorph = Morph.extend(function BoxMorph(superconstructor, rect) {
+		//console.log('BoxMorph ' + rect);
+		var shape = rect;
+		superconstructor(shape);
+	    });
+	    
+	    var ContainerMorph = BoxMorph.extend(function ContainerMorph(superconstructor, shape) {
+		//console.log('ContainerMorph ');
+		superconstructor(shape);
+	    });
+	    
+	    ContainerMorph.prototype.defineSlots({
+		getShape: {override: true, value: function(superconstructor) {
+		    console.log('hello from ContainerMorph.getShape');
+		    return superconstructor();
+		}},
+		getBounds: { override: false, value: function() {
+		    return this.getShape();
+		}}
+	    });
+	    
+	    
+	    var r = new Rectangle(10,10,10,10);
+	    var b = new BoxMorph(r);
+	    var c = new ContainerMorph(r);
+	    
+	    print('shape of c is ' + c.getShape());
+	    
+	    print(b instanceof BoxMorph);
+	    print(b instanceof Morph);
+	    print(c instanceof BoxMorph);
+	    
+	    print(b instanceof ContainerMorph);
+	    
+	    print(b instanceof Rectangle);
+	    print('same thing? ' + (Object.getPrototypeOf(b) === Object.getPrototypeOf(c)));
+	    print('Morph schema: ' + JSON.serialize(Morph.prototype.$schema));
+	    print('BoxMorph schema: ' + JSON.serialize(Object.getPrototypeOf(b).$schema));
+	    print('ContainerMorph schema: ' + JSON.serialize(Object.getPrototypeOf(c).$schema));
+	    
+	    
+	}
+	
+	test2();
+	
+	function test3() {
+	    
+	    print(JSON.serialize(fx.deepCopy({x: {a: 10, b:'bar'}, y:'foo'})));
+	    print(JSON.serialize(fx.deepCopy({x: {a: 10, b:'bar'}, y:'foo'}, ['b'])));
+	    var Base = fx.Object.extend(function(inherited, name, content) {
+		inherited();
+		this.toString = function() { return name };
+		Object.extend(this, content);
+	    });
+	    
+	    var x = new Base('x', {a:1, b: 2});
+	    var y = new Base('y', {a:3, b: 4, c: x});
+	    var z = new Base('z', {a: x, b: y, c: x});
+	    x.z = z;
+	    var copy1 = fx.deepCopy(z);
+	    //print(copy1.a.z.c);
+	    print(JSON.serialize(fx.shallowCopy({x:"Hello"})));
+	    print();
+	    
+	    var n4 = new Base('n4', {car: 4});
+	    var n3 = new Base('n3', {car: 3, cdr: n4});
+	    var n2 = new Base('n2', {car: 2, cdr: n3});
+	    var n1 = new Base('n1', {car: 1, cdr: n2, tag: 'foo', tag2: 'bar'});
+	    n4.cdr = n1;
+	    false && fx.visitAll([n1], function(owner, name) { 
+		var object = owner[name];
+		if (name && !(object instanceof Function)) 
+		    print(owner + "." + name + '=' + object) 
+		return object;
+	    });
+	    
+	    fx.visitAll([n1], function(owner, name, index) { 
+		var object = owner[name];
+		if (name && !(object instanceof Function)) {
+		if (object instanceof Object) {
+		    print('<field name=' + name + ' owner=' + index + "/>");
+		}
+		    //print(owner + "." + name + '=' + object + (index === undefined ? "" : '(' + index + ')'));
+		}
+		return object;
+	    });
+	}
+	
+	test3();
+	
+	
+    });
+
+}
