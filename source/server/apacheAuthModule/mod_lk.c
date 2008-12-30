f3bc1bf8bb1e182d633ef891dea6cca19d45aec6
Robert Krahn <robert.krahn@gmail.com>
2008-12-30 17:33:22 +0000

    Made wiki authorization configurable. To turn it on write UseLKWikiAuth On in a directory definition in a apache conf file.

--- source/server/apacheAuthModule/mod_lk.c
+++ source/server/apacheAuthModule/mod_lk.c
@@ -15,48 +15,16 @@
 // Apache core
 #include "httpd.h"
 #include "http_config.h"
-
 // For string manipulation
 #include <string.h>
 
-// authorization callback
-// if the requested file is not a xhtml file, don't modify authorization
-// if it is xhtml and it does not starts with the user name forbid writting the file
-// else allow writing the file
-static int mod_lk_wikiauth_handler (request_rec *r) {
-	fprintf(stderr,"mod_lk: An auth request was made. --> ");
+#define XHTML "xhtml"
+#define FALSE 0
+#define TRUE 1
 
-	// only handle PUT requests
-	// if (r->method_number != M_PUT)
-	// 	return DECLINED;
-
-	// extract filename
-	char *filename = strrchr(r->uri, '/') + 1;
-	if (filename == NULL) filename = r->uri;
-
-	fprintf(stderr, filename);
-	fprintf(stderr, "\n");
-	fflush(stderr);
-		
-	// is this an xhtml?
-	// --> better would be to use content_type but it is not initialized yet...
-	char *ending = strrchr(filename, '.') + 1;
-	fprintf(stderr, ending); fflush(stderr);
-	
-	if (ending == NULL || strcmp(ending, "xhtml") != 0) return DECLINED;
-	
-	fprintf(stderr, "XHTML found!!!\n"); fflush(stderr);
-	
-	// does the file starts with the user name?
-	char *usernameInFilename = strstr(filename, r->user);
-	if (usernameInFilename == NULL) return 403; // forbidden
-	
-	fprintf(stderr, "User name matches!\n");
-	fprintf(stderr, usernameInFilename);
-	fprintf(stderr, "\n"); fflush(stderr);
-
-	return OK;
-}
+// forward declarations
+module AP_MODULE_DECLARE_DATA lk_module;
+static int mod_lk_wikiauth_handler(request_rec*);
 
 // what callbacks should be called for request processing and configuration requests
 static void mod_lk_register_hooks (apr_pool_t *p) {
@@ -65,13 +33,65 @@ static void mod_lk_register_hooks (apr_pool_t *p) {
 	// ap_hook_handler(mod_lk_method_handler, NULL, NULL, APR_HOOK_LAST);
 }
 
+// config stuff
+typedef struct {
+  int useWikiAuth;
+} lk_module_config;
+static void *create_lk_module_config(apr_pool_t *p, server_rec *s) {
+	lk_module_config *cfg;
+	cfg = (lk_module_config*)apr_pcalloc(p, sizeof(lk_module_config));
+	cfg->useWikiAuth = FALSE;
+	return (void *) cfg;
+};
+static const command_rec lk_module_cmds[] = {
+	AP_INIT_FLAG(
+		"UseLKWikiAuth",
+		ap_set_flag_slot,
+		(void*)APR_OFFSETOF(lk_module_config, useWikiAuth),
+		OR_ALL,
+		"Something went wrong with UseLKWikiAuth"
+	), {NULL}
+};
+
 // name is important!
 module AP_MODULE_DECLARE_DATA lk_module = {
 	STANDARD20_MODULE_STUFF,
+	create_lk_module_config,
 	NULL,
 	NULL,
 	NULL,
-	NULL,
-	NULL,
+	lk_module_cmds,
 	mod_lk_register_hooks, // cb for hook registration
 };
+
+// authorization callback
+// if the requested file is not a xhtml file, don't modify authorization
+// if it is xhtml and it does not starts with the user name forbid writting the file
+// else allow writing the file
+static int mod_lk_wikiauth_handler (request_rec *r) {
+	// only handle PUT requests
+	// if (r->method_number != M_PUT)
+	// 	return DECLINED;
+
+	lk_module_config* cfg = ap_get_module_config(r->per_dir_config, &lk_module);
+	if (cfg->useWikiAuth == FALSE) return DECLINED; // check config
+	
+	// extract filename
+	char *filename = strrchr(r->uri, '/') + 1;
+	if (filename == NULL) filename = r->uri;
+
+	// is this an xhtml?
+	// --> better would be to use content_type but it is not initialized yet...
+	char *ending = strrchr(filename, '.') + 1;
+	if (ending == NULL || strcmp(ending, XHTML) != 0) return DECLINED;
+	
+	fprintf(stderr, "XHTML found!!!\n");
+	fprintf(stderr, filename); fflush(stderr);
+	
+	// does the file starts with the user name?
+	char *userInFilename = strstr(filename, r->user);
+	if (userInFilename == NULL || strcmp(userInFilename, filename) != 0) return 403; // forbidden
+	
+	fprintf(stderr, "User name matches!\n"); fflush(stderr);
+	return OK;
+}
