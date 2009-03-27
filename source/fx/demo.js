983b38f256626e7ce5f9369201b49d4b634c6ef2
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2009-03-27 01:40:35 +0000

    more copyright notices

--- source/fx/demo.js
+++ source/fx/demo.js
@@ -1,12 +1,25 @@
 /*
- * Copyright � 2006-2008 Sun Microsystems, Inc.
- * All rights reserved.  Use is subject to license terms.
- * This distribution may include materials developed by third parties.
- *  
- * Sun, Sun Microsystems, the Sun logo, Java and JavaScript are trademarks
- * or registered trademarks of Sun Microsystems, Inc. in the U.S. and
- * other countries.
- */ 
+ * Copyright (c) 2006-2009 Sun Microsystems, Inc.
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 
 load('browser.js');
