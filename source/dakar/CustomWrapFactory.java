b41ba37d69bf75d60a3b919ae493654355ee6969
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2009-05-23 01:52:50 +0000

    cleanup

--- source/dakar/CustomWrapFactory.java
+++ source/dakar/CustomWrapFactory.java
@@ -13,10 +13,28 @@ public class CustomWrapFactory extends WrapFactory {
 	Object javaObject;
 	Scriptable parent;
 	Scriptable prototype;
+	ArrayList<String> properties = new ArrayList<String>(); // could be shared based on type
 
-	public AbstractScriptable(Scriptable scope, Object javaObject) {
+	public AbstractScriptable(Scriptable scope, Object javaObject, Class type) {
 	    this.javaObject = javaObject;
 	    this.parent = scope;
+	    try {
+		Object content = this.getTarget(); 
+		// cache methods, check if content has the same class and if not, update properties.
+		if (content != null) {
+		    System.err.println("class " + content.getClass().getName());
+		    for (Method m : content.getClass().getMethods()) {
+			//System.err.println("method " + m.getName());
+			if (m.getName().startsWith("get$")) {
+			    properties.add(m.getName().substring(4));
+			}
+			// how about instance methods???
+		    }
+		    //if (properties.size() > 0) System.err.println("properties of " + content.getClass().getName() + " " + properties);
+		}
+	    } catch (Exception e) {
+		throw new RuntimeException(e);
+	    }
 	}
 	
 	public boolean hasInstance(Scriptable value) {
@@ -61,70 +79,30 @@ public class CustomWrapFactory extends WrapFactory {
 	public void put(int index, Scriptable start, Object value) {
 	    throw new RuntimeException();
 	}
-    }
-    
-
-    public static class ScriptableLocation  extends AbstractScriptable  {
-	ArrayList<String> properties = new ArrayList<String>(); // could be shared based on type
-
-	public ScriptableLocation(Scriptable scope, Object object, Class type) {
-	    super(scope, object);
-	    try {
-		Object content = ((ObjectLocation)object).get();
-		// cache methods, check if content has the same class and if not, update properties.
-		if (content != null) {
-		    System.err.println("class " + content.getClass().getName());
-		    for (Method m : content.getClass().getMethods()) {
-			//System.err.println("method " + m.getName());
-			if (m.getName().startsWith("get$")) {
-			    properties.add(m.getName().substring(4));
-			}
-			// how about instance methods???
-		    }
-		    //if (properties.size() > 0) System.err.println("properties of " + content.getClass().getName() + " " + properties);
-		}
-	    } catch (Exception e) {
-		throw new RuntimeException(e);
-	    }
-	}
 
 	public boolean has(String name, Scriptable start) {
 	    return this.properties.contains(name);
-	}
-
-	public String getClassName() {
-	    return "FXLocation";
 	}
 
 	public Object[] getIds() {
 	    return this.properties.toArray();
 	}
 
-	private ObjectLocation extractFieldVariable(String name) throws Exception {
-	    Object content = ((ObjectLocation)this.javaObject).get();
+	abstract Object getTarget();
+
+	ObjectLocation extractFieldVariable(String name) throws Exception {
+	    Object content = this.getTarget();
 	    Method getter = content.getClass().getMethod("get$" + name);
 	    ObjectLocation variable = (ObjectLocation)getter.invoke(content);
 	    return variable;
 	}
 
-	public Object get(int index, Scriptable start) {
-	    try {
-		if (this.javaObject instanceof SequenceLocation) {
-		    Object result = ((SequenceLocation)this.javaObject).get(index);
-		    // this will return naked FXObject, not Location
-		    //System.err.println("got result " + result);
-		    return Context.javaToJS(result, start);
-		}
-		return Scriptable.NOT_FOUND;
-	    } catch (Exception e) {
-		e.printStackTrace(System.err);
-		return Scriptable.NOT_FOUND;
-	    }
-	}
-
-
 	public Object get(String name, Scriptable start) {
 	    try {
+		if (name.equals("initialize$"))  { // FIXME ad hoc
+		    System.err.println("Trying to initialize through " + name);
+		    return new NativeJavaMethod(this.javaObject.getClass().getMethod(name), name);
+		}
 		ObjectLocation variable = this.extractFieldVariable(name);
 		System.err.println("GET " + name);
 		// hmm, no conversion to Scriptable here? is this automatic?
@@ -136,10 +114,10 @@ public class CustomWrapFactory extends WrapFactory {
 		return Context.javaToJS(variable, start); // FIXME start?
 	    } catch (Exception e) { 
 		System.err.println("not found getter " + name);
-		return Scriptable.NOT_FOUND;
+		return Context.getUndefinedValue();
 	    }
 	}
-	
+
 	public void put(String name, Scriptable start, Object value) {
 	    if (value instanceof NativeArray) {
 		NativeArray array = (NativeArray)value;
@@ -173,119 +151,61 @@ public class CustomWrapFactory extends WrapFactory {
 		e.printStackTrace(System.err);
 	    }
 	}
-    }
-
-    public static class ScriptableFXObject extends AbstractScriptable  {
-	ArrayList<String> properties = new ArrayList<String>(); // could be shared based on type
-	public ScriptableFXObject(Scriptable scope, FXObject object, Class type) {
-	    super(scope, object);
+	public Object get(int index, Scriptable start) {
 	    try {
-		// cache methods, check if content has the same class and if not, update properties.
-		if (object != null) {
-		    System.err.println("class " + object.getClass().getName());
-		    for (Method m : object.getClass().getMethods()) {
-			//System.err.println("method " + m.getName());
-			if (m.getName().startsWith("get$")) {
-			    properties.add(m.getName().substring(4));
-			} else if (m.getName().equals("initialize$")) {
-			    properties.add("initialize$");
-			}
-		    }
-		    if (properties.size() > 0) 
-			System.err.println("properties of " + object.getClass().getName() + " " + properties);
+		if (this.javaObject instanceof SequenceLocation) {
+		    Object result = ((SequenceLocation)this.javaObject).get(index);
+		    // this will return naked FXObject, not Location
+		    //System.err.println("got result " + result);
+		    return Context.javaToJS(result, start);
 		}
+		return Scriptable.NOT_FOUND;
 	    } catch (Exception e) {
-		throw new RuntimeException(e);
+		e.printStackTrace(System.err);
+		return Scriptable.NOT_FOUND;
 	    }
 	}
-
-	public boolean has(String name, Scriptable start) {
-	    return this.properties.contains(name);
+    }
+    
+    public static class ScriptableLocation  extends AbstractScriptable  {
+	public ScriptableLocation(Scriptable scope, Object object, Class type) {
+	    super(scope, object, type);
 	}
 
 	public String getClassName() {
 	    return "FXLocation";
 	}
 
-	public Object[] getIds() {
-	    return this.properties.toArray();
+	Object getTarget() {
+	    return ((ObjectLocation)javaObject).get();
+	}
+    }
+
+    public static class ScriptableFXObject extends AbstractScriptable  {
+
+	public ScriptableFXObject(Scriptable scope, FXObject object, Class type) {
+	    super(scope, object, type);
 	}
 
-	public Object get(int index, Scriptable start) {
-	    return Scriptable.NOT_FOUND;
-	}
-	
-	private ObjectLocation extractFieldVariable(String name) throws Exception {
-	    Object content = this.javaObject;
-	    Method getter = content.getClass().getMethod("get$" + name);
-	    ObjectLocation variable = (ObjectLocation)getter.invoke(content);
-	    return variable;
+	public String getClassName() {
+	    return "FXObject";
 	}
 
-	public Object get(String name, Scriptable start) {
-	    try {
-		if (name.equals("initialize$"))  { // FIXME ad hoc
-		    System.err.println("Trying to initialize through " + name);
-		    return new NativeJavaMethod(this.javaObject.getClass().getMethod(name), name);
-		}
-		
-		ObjectLocation variable = this.extractFieldVariable(name);
-		System.err.println("GET " + name);
-		// hmm, no conversion to Scriptable here? is this automatic?
-		Object content = variable.get(); // get will box the primitive?
-		if (content instanceof Boolean || content instanceof Number || content instanceof String) {
-		    System.err.println("variable was " + variable);
-		    return content;
-		}
-		return Context.javaToJS(variable, start); // FIXME start?
-	    } catch (Exception e) { 
-		System.err.println("not found getter " + name);
-		return Context.getUndefinedValue();
-	    }
-	}
-	
-	public void put(String name, Scriptable start, Object value) {
-	    if (value instanceof NativeArray) {
-		NativeArray array = (NativeArray)value;
-		int length = (int)array.getLength();
-		Object[] jarray = new Object[length];
-		// FIXME this assumes array of ObjectLocations!
-		for (int i = 0; i < length; i++) {
-		    jarray[i] = (FXObject)((org.mozilla.javascript.Wrapper)array.get(i, start)).unwrap();
-		}
-		value = Sequences.make(TypeInfo.Object, jarray, length);
-	    }
-	    try {
-		ObjectLocation variable = this.extractFieldVariable(name);
-		if (value instanceof org.mozilla.javascript.Wrapper) {
-		    // FIXME is there a better way???
-		    value = ((org.mozilla.javascript.Wrapper)value).unwrap();
-		}
-		System.err.println("variable " + variable + " new value " + value);
-		if (!(value instanceof Float) && (variable instanceof FloatLocation)) {
-		    // FIXME FIXME super ad-hoc
-		    value = new Float(((Number)value).floatValue()); 
-		} else if (value instanceof ObjectLocation) {
-		    // here's a place where two locations could be bound to each other?
-		    value = ((ObjectLocation)value).get();
-		}
-		variable.set(value); 
-		//System.err.println("!PUT " + name + ", " + getter + " " + variable.get());
-		return;
-	    } catch (Exception e) { 
-		e.printStackTrace(System.err);
-	    }
+	Object getTarget() {
+	    return javaObject;
 	}
     }
 
     public Object wrap(Context cx, Scriptable scope, Object obj, Class staticType) {
 	Class type = obj == null ? staticType : obj.getClass();
-	if (ObjectLocation.class.isAssignableFrom(type)) {
-	    System.err.println("custom wrapping " + type);
-	    return new ScriptableLocation(scope, obj, type);
-	} else if (FXObject.class.isAssignableFrom(type)) {
-	    System.err.println("FX custom wrapping " + type);
-	    return new ScriptableFXObject(scope, (FXObject)obj, type);
+	if (type != null) {
+	    if (ObjectLocation.class.isAssignableFrom(type)) {
+		System.err.println("custom wrapping " + type);
+		return new ScriptableLocation(scope, obj, type);
+	    } else if (FXObject.class.isAssignableFrom(type)) {
+		System.err.println("FX custom wrapping " + type);
+		return new ScriptableFXObject(scope, (FXObject)obj, type);
+	    }
 	}
 	return super.wrap(cx, scope, obj, staticType);
     }
