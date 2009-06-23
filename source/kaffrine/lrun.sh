5f379c30adaa123706a2cde7c6fae95fe50085ff
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2009-06-23 04:21:38 +0000

    more conveniences

--- source/kaffrine/lrun.sh
+++ source/kaffrine/lrun.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 lib=../../lib
+fxlib=/Library/Frameworks/JavaFX.framework/Versions/Current/lib
+
+exec java -javaagent:rhinifier.jar -Djava.ext.dirs=$fxlib/shared:$fxlib/desktop:ext  jline.ConsoleRunner com.sun.javafx.runtime.Main org.mozilla.javascript.tools.shell.Main $*
+
+
+
 #exec /usr/local/soylatte/bin/java -classpath $lib/Scenario.jar:$lib/js.jar org.mozilla.javascript.tools.shell.Main $*
 #exec java -classpath $lib/Scenario-0.6.jar:$lib/js.jar org.mozilla.javascript.tools.shell.Main $*
-fxlib=/Library/Frameworks/JavaFX.framework/Versions/Current/lib
-#fxlib=/Library/Frameworks/JavaFX.framework/Versions/1.1/lib
-
-exec java -javaagent:rhinifier.jar -Djava.ext.dirs=$fxlib/shared:$fxlib/desktop:ext -classpath $lib/js.jar:$lib/jline.jar:. jline.ConsoleRunner com.sun.javafx.runtime.Main org.mozilla.javascript.tools.shell.Main -version 170  $*
-
-
-
-
 #exec java -Djava.ext.dirs=$fxlib/shared:$fxlib/desktop -classpath $lib/js.jar:$lib/jline.jar:. jline.ConsoleRunner Spawn $*
 
 #exec java -classpath $lib/javafxrt.jar:$lib/Scenario.jar:$lib/javafxgui.jar:$lib/js.jar:$lib/jline.jar:. jline.ConsoleRunner org.mozilla.javascript.tools.shell.Main $*
