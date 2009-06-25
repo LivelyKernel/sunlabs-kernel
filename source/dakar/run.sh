227ef6ce4a0dbaf5491f396c01a5e31f434b8ef2
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2009-06-25 03:48:42 +0000

    version fixes

--- source/dakar/run.sh
+++ source/dakar/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 lib=../../lib
 #exec /usr/local/soylatte/bin/java -classpath $lib/Scenario.jar:$lib/js.jar org.mozilla.javascript.tools.shell.Main $*
-#exec java -classpath $lib/Scenario-0.6.jar:$lib/js.jar org.mozilla.javascript.tools.shell.Main $*
-fxlib=/Library/Frameworks/JavaFX.framework/Versions/Current/lib
-exec java -classpath $fxlib/desktop/Scenario.jar:$lib/js.jar:$fxlib/shared/javafxrt.jar:$fxlib/desktop/javafxgui.jar:$lib/jline.jar org.mozilla.javascript.tools.shell.Main $*
+#exec java -classpath $lib/Scenario.jar:$lib/js.jar org.mozilla.javascript.tools.shell.Main $*
+fxlib=/Library/Frameworks/JavaFX.framework/Versions/1.1/lib
+exec java -classpath $lib/Scenario.jar:$lib/js.jar:$lib/javafxrt.jar:$lib/javafxgui.jar:$lib/jline.jar jline.ConsoleRunner org.mozilla.javascript.tools.shell.Main $*
