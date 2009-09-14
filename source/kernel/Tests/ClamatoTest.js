1e3fe9b1bf4d85c38a89634ae6ae80613c1ffccb
Robert Krahn <robert.krahn@gmail.com>
2009-09-14 11:00:49 +0000

    Clamato: parsing keyword messages

--- source/kernel/Tests/ClamatoTest.js
+++ source/kernel/Tests/ClamatoTest.js
@@ -20,16 +20,41 @@ test01ParseUnaryMessageSend: function() {
 test02ParseBinaryMessageSend: function() {
 	var src = 'x ++ 1';
 	var result = this.parse('expression', src);
-xx=result;
 	this.assert(result.isMessage, 'not a message node');
 	this.assert(result.isBinary , 'not binary');
 	this.assertEqual('x', result.receiver.name, 'wrong receiver name');
 	this.assertEqual('++', result.messageName, 'wrong message name');
 	this.assertEqual(1, result.arguments.length);
-	this.assert(1, result.arguments.length);
 	this.assert(result.arguments.first().isLiteral);
 	this.assertEqual(1, result.arguments.first().value);
 },
+test03aKeywordyMessageSend: function() {
+	var src = 'x foo: -42';
+	var result = this.parse('expression', src);
+	this.assert(result.isMessage, 'not a message node');
+	this.assert(result.isKeyword , 'not keyword');
+	this.assertEqual('foo:', result.messageName, 'wrong message name');
+	this.assertEqual(1, result.arguments.length);
+	this.assert(result.arguments.first().isLiteral);
+	this.assertEqual(-42, result.arguments.first().value);
+},
+test03bKeywordyMessageSend: function() {
+	var src = 'x foo: 42 bar: blupf baz: 23';
+	var result = this.parse('expression', src);
+	this.assert(result.isMessage, 'not a message node');
+	this.assert(result.isKeyword , 'not keyword');
+	this.assertEqual('foo:bar:baz:', result.messageName, 'wrong message name');
+	this.assertEqual(3, result.arguments.length);
+	this.assert(result.arguments[0].isLiteral);
+	this.assertEqual(42, result.arguments[0].value);
+	this.assert(result.arguments[1].isVariable);
+	this.assertEqual('blupf', result.arguments[1].name);
+	this.assert(result.arguments[2].isLiteral);
+	this.assertEqual(23, result.arguments[2].value);
+},
+
+
+
 
 });
 
