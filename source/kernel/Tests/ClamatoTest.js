36894a8e47630a8b9f9fbf5ec1752392424c92c4
Robert Krahn <robert.krahn@gmail.com>
2009-09-14 16:35:25 +0000

    recognizing clamato st expressions

--- source/kernel/Tests/ClamatoTest.js
+++ source/kernel/Tests/ClamatoTest.js
@@ -15,7 +15,7 @@ test01ParseUnaryMessageSend: function() {
 	this.assert(result.receiver.isVariable, 'receiver is not a variable');
 	this.assertEqual('x', result.receiver.name, 'wrong receiver name');
 	this.assertEqual('foo', result.messageName, 'wrong message name');
-	this.assertEqual(null, result.arguments);
+	this.assertEqual(null, result.args);
 },
 test02ParseBinaryMessageSend: function() {
 	var src = 'x ++ 1';
@@ -24,9 +24,9 @@ test02ParseBinaryMessageSend: function() {
 	this.assert(result.isBinary , 'not binary');
 	this.assertEqual('x', result.receiver.name, 'wrong receiver name');
 	this.assertEqual('++', result.messageName, 'wrong message name');
-	this.assertEqual(1, result.arguments.length);
-	this.assert(result.arguments.first().isLiteral);
-	this.assertEqual(1, result.arguments.first().value);
+	this.assertEqual(1, result.args.length);
+	this.assert(result.args.first().isLiteral);
+	this.assertEqual(1, result.args.first().value);
 },
 test03aKeywordyMessageSend: function() {
 	var src = 'x foo: -42';
@@ -34,9 +34,9 @@ test03aKeywordyMessageSend: function() {
 	this.assert(result.isMessage, 'not a message node');
 	this.assert(result.isKeyword , 'not keyword');
 	this.assertEqual('foo:', result.messageName, 'wrong message name');
-	this.assertEqual(1, result.arguments.length);
-	this.assert(result.arguments.first().isLiteral);
-	this.assertEqual(-42, result.arguments.first().value);
+	this.assertEqual(1, result.args.length);
+	this.assert(result.args.first().isLiteral);
+	this.assertEqual(-42, result.args.first().value);
 },
 test03bKeywordyMessageSend: function() {
 	var src = 'x foo: 42 bar: blupf baz: 23';
@@ -44,14 +44,29 @@ test03bKeywordyMessageSend: function() {
 	this.assert(result.isMessage, 'not a message node');
 	this.assert(result.isKeyword , 'not keyword');
 	this.assertEqual('foo:bar:baz:', result.messageName, 'wrong message name');
-	this.assertEqual(3, result.arguments.length);
-	this.assert(result.arguments[0].isLiteral);
-	this.assertEqual(42, result.arguments[0].value);
-	this.assert(result.arguments[1].isVariable);
-	this.assertEqual('blupf', result.arguments[1].name);
-	this.assert(result.arguments[2].isLiteral);
-	this.assertEqual(23, result.arguments[2].value);
+	this.assertEqual(3, result.args.length);
+	this.assert(result.args[0].isLiteral);
+	this.assertEqual(42, result.args[0].value);
+	this.assert(result.args[1].isVariable);
+	this.assertEqual('blupf', result.args[1].name);
+	this.assert(result.args[2].isLiteral);
+	this.assertEqual(23, result.args[2].value);
 },
+test04bChainedBinaryMessages: function() {
+	var src = 'x + 1 * 2';
+	var result = this.parse('expression', src);
+	this.assert(result.isMessage, 'not a message node');
+	this.assert(result.isBinary , 'not binary');
+	this.assertEqual('*', result.messageName, 'wrong message name');
+	this.assertEqual(2, result.args[0].value, 'wrong arg');
+	result = result.receiver;
+	this.assert(result.isMessage, 'not a message node 2');
+	this.assert(result.isBinary , 'not binary 2');
+	this.assertEqual('+', result.messageName, 'wrong message name 2');
+	this.assertEqual(1, result.args[0].value, 'wrong arg 2');
+	this.assert(result.receiver.isVariable , 'wrong receiver 2');
+},
+
 
 
 
