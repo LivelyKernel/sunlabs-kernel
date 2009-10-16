f97d6c19847a6457be005f5d52012fe093f06326
Robert Krahn <robert.krahn@gmail.com>
2009-10-16 11:46:54 +0000

    made textarea for copying visible again for making debugging copy behavior simpler

--- source/kernel/Core.js
+++ source/kernel/Core.js
@@ -5536,7 +5536,8 @@ ClipboardHack = {
 		buffer.setAttribute("cols","1");
 		buffer.setAttribute("rows","1");
 		buffer.setAttribute("id","copypastebuffer");
-		buffer.setAttribute("style","position:absolute;z-index: -400;left:0px; top:1px; width:1px; height:1px;");
+		// buffer.setAttribute("style","position:absolute;z-index: -400;left:0px; top:1px; width:1px; height:1px;");
+		buffer.setAttribute("style","position:absolute;z-index: 5;left:0px; top:1px; width:1px; height:1px;");
 		buffer.textContent = "NoText";
 		var outerBody = Global.document.body || Global.parent.document.body;
 		outerBody.appendChild(buffer);
