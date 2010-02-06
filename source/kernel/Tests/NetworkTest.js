8dc94ed643928a2d86411523cd235db7b36770e6
Robert Krahn <robert.krahn@gmail.com>
2010-02-06 22:10:57 +0000

    added network test. Created it a while ago but didn't commited it. Currently tests only some URL behavior.

---
+++ source/kernel/Tests/NetworkTest.js
@@ -0,0 +1,31 @@
+module('lively.Tests.NetworkTest').requires('lively.TestFramework').toRun(function() {
+	
+TestCase.subclass('lively.Tests.NetworkTest.URLTest', {
+	
+	testEnsureAbsoluteURL1: function() {
+		var urlString = 'http://livelykernel.sunlabs.com/repository/lively-wiki/index.xhtml';
+		var result = URL.ensureAbsoluteURL(urlString);
+		this.assertEqual(urlString, result.toString());
+		
+		urlString = 'http://localhost/lively/index.xhtml';
+		result = URL.ensureAbsoluteURL(urlString);
+		this.assertEqual(urlString, result.toString());
+	},
+	
+	testEnsureAbsoluteURL2: function() {
+		var urlString = 'index.xhtml';
+		var result = URL.ensureAbsoluteURL(urlString);
+		var expected = URL.source.getDirectory().toString() + urlString;
+		this.assertEqual(expected, result.toString());
+	},
+
+	testEnsureAbsoluteURL3: function() {
+		var urlString = 'bla/http/blupf.xhtml';;
+		var result = URL.ensureAbsoluteURL(urlString);
+		var expected = URL.source.getDirectory().toString() + urlString;
+		this.assertEqual(expected, result.toString());
+	}
+	
+});
+	
+});
