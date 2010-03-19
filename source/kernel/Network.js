7272db2eee293ba2913d4d6031a6029e903c5499
Jens Lincke <jenslincke@gmail.com>
2010-03-19 14:44:14 +0000

    5min to go

--- source/kernel/Network.js
+++ source/kernel/Network.js
@@ -1037,6 +1037,7 @@ Object.extend(FileDirectory, {
 Object.sublcass("FileUploadHelper", {
 	initialize: function() {
 		this.uploadFileMaxSize = 1024000;
+		this.prefix = ""
 	},
 	
 	sendFile: function(handler){
@@ -1082,10 +1083,39 @@ Object.sublcass("FileUploadHelper", {
 		}
 	},
 
+	// function to upload multiple files via handler
+	sendMultipleFiles: function sendMultipleFiles(handler){
+	    var length = handler.files.length,
+	        i = 0,
+	        onload = handler.onload;
+	    handler.current = 0;
+	    handler.total = 0;
+	    handler.sent = 0;
+	    while(handler.current < length)
+	        handler.total += handler.files[handler.current++].fileSize;
+	    handler.current = 0;
+	    if(length){
+	        handler.file = handler.files[handler.current];
+	        sendFile(handler).onload = function(rpe, xhr){
+	            if(++handler.current < length){
+	                handler.sent += handler.files[handler.current - 1].fileSize;
+	                handler.file = handler.files[handler.current];
+	                sendFile(handler).onload = arguments.callee;
+	            } else if(onload) {
+	                handler.onload = onload;
+	                handler.onload(rpe, xhr);
+	            }
+	        };
+	    };
+	    return  handler;
+	},
+	
 	urlForFileName: function(file) {
-		return file
+		return this.prefix + file
 	}
 });
+
+
 
 
 console.log('loaded Network.js');
