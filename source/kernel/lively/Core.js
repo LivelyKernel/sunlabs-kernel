65f476fb108302746790090b7aa6b3c15a6b6f21
Robert Krahn <robert.krahn@gmail.com>
2010-04-14 21:40:23 +0000

    label change

--- source/kernel/lively/Core.js
+++ source/kernel/lively/Core.js
@@ -4820,9 +4820,9 @@ WorldMorph.addMethods({
 				};
 				world.prompt('Wiki base URL?', cb, URL.source.getDirectory().toString());
 				})}],
-			["Switch code base...", function(evt) { require('lively.ide').toRun(function(unused, ide) {
+			["Switch System browser directory...", function(evt) { require('lively.ide').toRun(function(unused, ide) {
 				var cb = function(input) { ide.startSourceControl().switchCodeBase(new URL(input)) };
-				world.prompt('New code base?', cb, URL.source.getDirectory().toString());
+				world.prompt('Enter System browser directory (URL)', cb, URL.source.getDirectory().toString());
 				})}],				
 			["File Browser", function(evt) { new FileBrowser().openIn(world) }],
 			["Object Hierarchy Browser", function(evt) { new ObjectBrowser().openIn(world); }],	
