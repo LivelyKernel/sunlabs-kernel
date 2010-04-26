f5ef1c896faef33c4a4cf6362f295780e1c88a9e
Robert Krahn <robert.krahn@gmail.com>
2010-04-26 15:55:27 +0000

    bugfix when creating a textmorph

--- source/kernel/lively/Core.js
+++ source/kernel/lively/Core.js
@@ -4838,8 +4838,6 @@ WorldMorph.addMethods({
 			["Ellipse", function(evt) { world.addMorph(Morph.makeCircle(evt.point(), 25)); }],
 			["TextMorph", function(evt) { 
 				var text = new TextMorph(evt.point().extent(pt(120, 10)), "This is a TextMorph");
-				if (Global.UndoLayer)
-					text.setWithLayers([UndoLayer]);
 				world.addMorph(text);}],
 			["Star",function(evt) { world.addMorph(Morph.makeStar(evt.point()))}],
 			["Heart", function(evt) { world.addMorph(Morph.makeHeart(evt.point()))}],
