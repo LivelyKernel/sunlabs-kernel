b929b77c35ac43adcfc6f1dc9e69dde79d4f2028
Jens Lincke <jenslincke@gmail.com>
2010-04-26 14:36:34 +0000

    added pageNavigationName to config

--- source/kernel/lively/defaultconfig.js
+++ source/kernel/lively/defaultconfig.js
@@ -346,3 +346,5 @@ if (false) {
 
 Config.confirmNavigation = false; // don't show confirmation dialog when navigating a link
 Config.useAltAsCommand = false; // User Platform Keys (Ctrl und Windows and Meta under Mac as command key)
+
+Config.pageNavigationName = "nothing"
