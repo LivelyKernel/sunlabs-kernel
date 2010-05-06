4d16453a440fd3a10cbfff6d6aa58d0a479c2072
Jens Lincke <jenslincke@gmail.com>
2010-05-06 09:23:39 +0000

    refactored test runner extensions

--- source/kernel/lively/TestRunnerExtensions.js
+++ source/kernel/lively/TestRunnerExtensions.js
@@ -2,78 +2,76 @@
  * Some minor benchmarking extensions for the TestFrameWork separated with layers
  *
  */
-
-
 module('lively.TestRunnerExtensions').requires('lively.Helper', 'cop.Layers', 'lively.TestFramework').toRun(function() {
 	
-createLayer("TimeTestRunLayer");
+createLayer("TimeEachTestLayer");
 
-layerClass(TimeTestRunLayer, TestCase, {
+layerClass(TimeEachTestLayer, TestCase, {
 	runTest: function(proceed) {
-		var profileName = "runTest " + this.currentSelector 
-		console.profile(profileName);
 		var start = (new Date()).getTime();	
 		proceed();
 		var time = (new Date()).getTime() - start;
-		console.profileEnd(profileName);
-		TimeTestRunLayer.setTimeOfTestRun(this.constructor.name, this.currentSelector, time)
-	}
-})
+		this.result.setTimeOfTestRun(this.currentSelector, time)
+	},
+});
 
-layerClass(TimeTestRunLayer, TestRunner, {
+layerClass(TimeEachTestLayer, TestResult, {
+
+	setTimeOfTestRun: function(proceed, selector, time) {
+		if (!this.timeOfTestRuns)
+			this.timeOfTestRuns = {};
+		this.timeOfTestRuns[selector] = time;
+	},
+
+	getSortedTimesOfTestRuns: function(proceed) {
+		var times = this.timeOfTestRuns
+		if(!times) return;	
+		var sortedTimes = Object.keys(times).collect(function(eaSelector) {
+			return [times[eaSelector], eaSelector]
+		}).sort(function(a, b) {return a[0] - b[0]});
+		return sortedTimes.collect(function(ea) {return ea.join("\t")}).join("\n")
+	}
+});
+
+
+layerClass(TimeEachTestLayer, TestRunner, {
 	setResultOf: function(proceed, testObject) {
 		proceed(testObject);
-		console.log( "\nTime TestRuns:\n" + TimeTestRunLayer.getSortedTimeOfTestRuns());
-		TimeTestRunLayer.timeOfTestRuns = {} // clear the run...
-	}
+		WorldMorph.current().setStatusMessage("\nTestRun: " + testObject.constructor.type + "\n" +
+			testObject.result.getSortedTimesOfTestRuns(), Color.blue, 10);
+		testObject.timeOfTestRuns = {} // clear the run...
+	},
 })
 
-createLayer("DeployTimeTestRunLayer");
-enableLayer(DeployTimeTestRunLayer);
+createLayer("TimeTestLayer");
+enableLayer(TimeTestLayer);
 
-// TimeTestRunLayer should only be active, when the controll flow goes throuh runTests...
-// DeployTimeTestRunLayer is used to separate the concern, but not to specify additional dynamic behavior
+layerClass(TimeTestLayer, TestRunner, {
+	
+	layersForTestRun: function() {
+		var layers = [TimeEachTestLayer];
+		if (Config.profileTestRuns)
+			layers.push(ProfileEachTestLayer)
+		return layers
+	},
 
-layerClass(DeployTimeTestRunLayer, TestRunner, {
 	runSelectedTestCase: function(proceed) {
-		withLayers([TimeTestRunLayer], function() {
+		withLayers(this.layersForTestRun(), function() {
 			proceed()
 		})
 	}
 })
 
-// Private Helper Functions
-TimeTestRunLayer.setTimeOfTestRun = function(className, selector, time) {
-	if (!this.timeOfTestRuns)
-		this.timeOfTestRuns = {};
-		
-	if(!this.timeOfTestRuns[className])
-		this.timeOfTestRuns[className]={};
-	
-	this.timeOfTestRuns[className][selector] = time;
-};
+createLayer("ProfileEachTestLayer");
+layerClass(ProfileEachTestLayer, TestCase, {
+	runTest: function(proceed) {
+		var profileName = "profile "  + this.currentSelector 
+		console.profile(profileName);
+		proceed();
+		console.profileEnd(profileName);
+	},
+});
 
-TimeTestRunLayer.getSortedTimeOfTestRuns = function() {
-	if(!this.timeOfTestRuns)
-		return;
-	
-	var result = []
-	Object.keys(this.timeOfTestRuns).each(function(eaClass) {
-		Object.keys(this.timeOfTestRuns[eaClass]).each(function(eaSelector) {
-			result.push([this.timeOfTestRuns[eaClass][eaSelector], eaClass, eaSelector])
-		}, this)
-	}, this);
-	
-	result = result.sort(function(a, b) {return a[0] - b[0]});
-	var string = "";
-	result.each(function(ea) {
-		string = string + ea[0] + "\t" + ea[1] + ">>" + ea[2] + "\n"
-	})
-	return string 
-}
-
-
-
-
+Config.profileTestRuns = true
 
 });
