8fa0e4afcbc0b6b045771a4acdb91d1f5e02a602
Robert Krahn <robert.krahn@gmail.com>
2010-08-30 14:42:39 +0000

    SandboxServer refactoring

--- source/kernel/server/nodejs/SandboxServer.js
+++ source/kernel/server/nodejs/SandboxServer.js
@@ -20,19 +20,36 @@ var Script = process.binding('evals').Script;
 
 var sandboxes = {};
 
-function setupSandbox() {
-	var sandbox = {require: require, Object: Object, process: process, Script: Script};
-	sandbox.Global = sandbox
-	Script.runInNewContext('sys = require("sys"); require("./miniprototype"); require("./Base")', sandbox);
-	return sandbox;
-}
+Object.subclass('Sandbox', {
+
+	initialize: function() {
+		this.evalContext = this.setupContext();
+	},
+
+	setupContext: function() {
+		var ctxt = {require: require, Object: Object, process: process, Script: Script};
+		ctxt.Global = ctxt
+		Script.runInNewContext('sys = require("sys"); require("./miniprototype"); require("./Base")', ctxt);
+		return ctxt;
+	},
+
+	eval: function(src) {
+		var result = Script.runInNewContext(src, this.evalContext);
+		// var result = Script.runInThisContext(src);
+		if (Object.isFunction(result)) result = result.toString();
+		return result;
+	},
+
+	evalToJsonString: function(src) {
+			var result = this.eval(src)
+			return JSON.stringify({result: result});
+	},
+
+});
 
 livelyServer.AbstractHandler.subclass('SandboxHandler', {
 
 	port: 8084,
-	
-	initialize: function() {
-	},
 	
 	run: function(request, response, content) {
 		
@@ -40,61 +57,20 @@ livelyServer.AbstractHandler.subclass('SandboxHandler', {
 		var source = json.src;
 		var id = json.id
 		
-		if (!source) {
-			this.error(response, 'Cannot find field "src" in json');
-			return
-		}
-		
-		if (!id) {
-			this.error(response, 'Cannot find field "id" in json');
-			return
-		}
-			
-		if (!sandboxes[id]) sandboxes[id] = setupSandbox();
+		if (!source) { this.error(response, 'Cannot find field "src" in json'); return }
+		if (!id) { this.error(response, 'Cannot find field "id" in json'); return }
 
-		sys.puts('Evaluating: ' + source);
+		if (!sandboxes[id]) sandboxes[id] = new Sandbox();
 
-		var jsonString;
 		try {
-			var result = Script.runInNewContext(source, sandboxes[id]);
-			// var result = Script.runInThisContext(source);
-			if (Object.isFunction(result)) result = result.toString();
-			jsonString = JSON.stringify({result: result});
+			var jsonString = sandboxes[id].evalToJsonString(source)
 		} catch(e) {
 			this.error(response, String(e))
 		}
-
-		sys.puts('...results in ' + jsonString);
-				
-		// sys.puts(sys.inspect(sandbox));
-
-		// var self = this;
-		// 
-		// var errorHandler = function (err) {
-		// 	try {
-		// 		self.cleanup();
-		// 	} finally {
-		// 		sys.puts('Caught exception: ' + err + '\n' + err.stack);
-		// 		self.error(response, 'Cannot process request because: ' + err + '\n' + err.stack );
-		// 		process.removeListener('uncaughtException', errorHandler);
-		// 	}
-		// }
-		// process.addListener('uncaughtException', errorHandler);
-		// 
-		// var json = JSON.parse(content.toString());
-		// var dir = json.directoryURL;
-		// var texFile = json.texFile;
-		// var resultURL = json.resultURL;
-		// // sys.puts('Server got ' + JSON.stringify(json));
-		// 
-		// this.pdfCreator.downloadAndCompile(dir, texFile, resultURL, function() {
-		// 	response.writeHead(200, {'Content-Type': 'text/plain'});
-		// 	response.end(resultURL);
-		// });
+		sys.puts('Evaluating: ' + source + ' results in ' + jsonString);
 
 		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.end(jsonString);
-		
+		response.end(jsonString);		
 	},
 	
 });
