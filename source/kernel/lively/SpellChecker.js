b77733cb9317abf0efc4e27ac7f9323f3fb8547e
Jens Lincke <jenslincke@gmail.com>
2010-09-09 16:00:21 +0000

    added spellchecker ignore list that is stored in localStorage

--- source/kernel/lively/SpellChecker.js
+++ source/kernel/lively/SpellChecker.js
@@ -37,8 +37,37 @@ Object.subclass("SpellChecker", {
 		console.log("I got something" + request.getResponseText());
  
 	}
-})
+});
 
+Object.extend(SpellChecker, {
+
+	genIngoreListName: function(lang) {
+		return 'SpellCheckingIgnoreList' + (lang || "en").capitalize()
+	},
+
+	getIgnoreList: function(lang) {
+		// JSON.stringify(['Hello'])
+		//localStorage['SpellCheckingIgnoreListTest']
+		var s = localStorage[this.genIngoreListName(lang)];
+		if (s) {
+			return JSON.parse(s)
+		} else {
+			return []
+		}
+	},
+
+	setIgnoreList: function(list, lang) {
+		localStorage[this.genIngoreListName(lang)] = JSON.stringify(list)		
+	},
+	
+	addIgnoreWord: function(word, lang) {
+		var list = this.getIgnoreList(lang)
+		if ( ! list.include(word)) {
+			list.push(word)
+			this.setIgnoreList(list, lang)	
+		}
+	},
+})
 
 cop.create('SpellCheckerLayer')
 .refineClass(TextMorph, {
@@ -50,22 +79,32 @@ cop.create('SpellCheckerLayer')
 		this.replaceSelectionWith(suggestion);
 		this.emphasizeFromTo({color: null, spellchecksuggestions: null }, offset, offset + suggestion.length)
 	},
+	
+	ignoreWordWhileSpellChecking: function(proceed, word) {
+		SpellChecker.addIgnoreWord(word, this.getSpellCheckLang());
+
+	},
 
 	morphMenu: function(proceed, evt) {
 		var spellCheck = this.spellCheckUnderMouse(evt);
 		var menu = proceed(evt);
 		var self = this;
-		if (menu && spellCheck && spellCheck.suggestions) {
-			var suggestions = spellCheck.suggestions;
+		if (menu && spellCheck ) {
 			menu.addItem(["-----", function(){ }], 0 );
-			console.log("spellCheck: " + suggestions)
-			suggestions.split('	').reverse().each(function(ea) {
-				menu.addItem([ea, function() { 
-					self.correctWithSuggestion(spellCheck.offset, spellCheck.length, ea)
-				}], 0 );
-			})
+			menu.addItem(["ignore \"" + spellCheck.word + '" in ' + this.getSpellCheckLang(), function() { 
+				self.ignoreWordWhileSpellChecking(spellCheck.word)
+			}], 0 );
+			var suggestions = spellCheck.suggestions;
+			if (suggestions) {
+				suggestions.split('	').reverse().each(function(ea) {
+					menu.addItem([ea, function() { 
+						self.correctWithSuggestion(spellCheck.offset, spellCheck.length, ea)
+					}], 0 );
+				})
+			}
 		}
 		menu.addItem(["check spelling", this.spellCheckAll])
+		menu.addItem(["disable spellchecking", this.unspellCheckAll])
 		return menu
 	},
 
@@ -75,35 +114,65 @@ cop.create('SpellCheckerLayer')
 
 	spellCheckAll: function() {
 		var checker = new SpellChecker();
-		var xmlString = checker.querySpellCheckService(this.textString, this.getSpellCheckLang());
+		var lang = this.getSpellCheckLang();
+		var xmlString = checker.querySpellCheckService(this.textString, lang);
 		if (!xmlString) {
 			throw new Error('Spell Checking: no response')
 		};
 		var self = this;
 		var xml = new DOMParser().parseFromString(xmlString, "text/xml");
+		var ignoreList = SpellChecker.getIgnoreList(lang)
 		corrections = checker.extractSpellCheck(xml)
 		corrections.each(function(ea){
 			var a = ea.offset;
 			var b = a + ea.length ;
-			console.log("spellCheck = " + ea);
-			var style = {color: "red",
-					spellchecksuggestions: ea.suggestions };
-			self.emphasizeFromTo(style,  a, b);
-			console.log("color from " + a + " to " + b)
-		})
+			var word = this.textString.slice(a, b)
+			// console.log("spellCheck " + word);
+			if (!ignoreList.include(word)) {
+				var style = {color: "red",
+						spellchecksuggestions: ea.suggestions };
+				self.emphasizeFromTo(style,  a, b);
+				console.log("color from " + a + " to " + b)
+			}
+		}, this)
 	},
-
+	unspellCheckAll: function() {
+		console.log('disable spell checking')
+		if (!this.textStyle)
+			return;
+		for (var i=0; i< this.textStyle.values.length; i++) {
+			var style = this.textStyle.values[i];
+			console.log('style ' + style.spellchecksuggestions)
+			
+			if ('spellchecksuggestions' in style) {
+				console.log('delete style ')
+				delete style.spellchecksuggestions;
+				delete style.color;
+			}
+			
+		}
+		this.textStyle = this.textStyle.coalesce();
+		this.composeAfterEdits()
+	},
  
 	spellCheckUnderMouse: function(proceed, evt) {	 
 		if (!this.textStyle) return null;
 		var charIx = this.charOfPoint(this.localize(evt.mousePoint));
 		var style = this.textStyle.valueAt(charIx);
+		if (! ('spellchecksuggestions' in style) )
+			return;
 		// we have to compute it, because it changes.....
 		var mark = this.textStyle.markAt(charIx)
+		var offset = charIx - mark.offset;
+		var length = this.textStyle.runs[mark.runIndex] - 1;
+		var word = this.textString.slice(offset, offset + length);
+		// console.log("word " + word + "offset " + offset + " length " + length )
+		var suggestions = style.spellchecksuggestions;
 		return {
-			offset: charIx - mark.offset,
-			length: this.textStyle.runs[mark.runIndex] - 1,
-			suggestions: style.spellchecksuggestions
+			word: word,
+			offset: offset,
+			length: length,
+			suggestions: suggestions
 		};		  
 	},
 
@@ -119,23 +188,18 @@ TestCase.subclass("lively.SpellChecker.SpellCheckerTest", {
 		var expected = '<?xml version="1.0" encoding="UTF-8"?><spellresult error="0" clipped="0" charschecked="13"><c o="10" l="3" s="0">text	TX	ext</c></spellresult>'
 	
 		var result = checker.querySpellCheckService("This is a txt");
-
 		this.assertEqual(result, expected, "wrong xml");	
 	},
 
 	testQuerySpellCheckServiceGerman: function() {
-
 		var self = this;
 		var checker = new SpellChecker();
-		
 		var expected = '<?xml version="1.0" encoding="UTF-8"?><spellresult error="0" clipped="0" charschecked="18"><c o="9" l="4" s="1">kein	Kai	Kamin	Karin	Hain</c><c o="14" l="4" s="1">Text	Axt	Tat	Taft	Takt	Taxi</c></spellresult>'
 	
 		var result = checker.querySpellCheckService("Dies ist kain Taxt", 'de');
 
 		this.assertEqual(result, expected, "wrong xml");	
 	},
-
-
 
 	testExtractSuggestions: function() {
 
@@ -150,7 +214,22 @@ TestCase.subclass("lively.SpellChecker.SpellCheckerTest", {
 		this.assert(spellChecks[0].length, " no length");
 		this.assert(spellChecks[0].confidence, " no confidence");
 		this.assert(spellChecks[0].suggestions, " no suggestions");
-	}
+	},
+
+	testSetGetIngoreList: function() {
+		SpellChecker.setIgnoreList(["TestWord"],"test")
+		
+		var list = SpellChecker.getIgnoreList("test")
+		this.assertEqual(list[0], "TestWord")
+	},
+	
+	testAddIgnoreWord: function() {
+		SpellChecker.setIgnoreList([],"test")
+		SpellChecker.addIgnoreWord("TestWord2","test")
+		
+		var list = SpellChecker.getIgnoreList("test")
+		this.assertEqual(list[0], "TestWord2")
+	},
 });
 
 });
