620bdedf0e33aed3e41aede1e1784d673552f757
Robert Krahn <robert.krahn@gmail.com>
2010-10-05 12:03:42 +0000

    Main - categorized main and support for JSON world loading

--- source/kernel/lively/Main.js
+++ source/kernel/lively/Main.js
@@ -39,7 +39,25 @@ Object.extend(Global, {
 	},	
 });
 
-Object.subclass('lively.Main.Loader', {
+Object.subclass('lively.Main.Loader',
+'properties', {
+	connections: ['finishLoading'],
+},
+'testing', {
+
+	documentHasSerializedMorphs: function(doc) {
+	    var nodes = doc.getElementsByTagName("g");
+		return nodes && nodes.length > 0;
+		// nodes[0].getAttribute("type") == "WorldMorph"; // world is not always serialized
+	},
+	findJSONWorld: function() {
+		var meta = document.getElementById('LivelyJSONWorld')
+		if (!meta) return null;
+		return meta.textContent;
+	},
+
+},
+'preparation', {
 
 	configPatches: function() {
 		// Class browser visibility can be overridden with Config.browserAnyway
@@ -62,12 +80,6 @@ Object.subclass('lively.Main.Loader', {
 		if(!WindowMorph) { WindowMorph = function() {} }
 	},
 
-	documentHasSerializedMorphs: function(doc) {
-	    var nodes = doc.getElementsByTagName("g");
-		return nodes && nodes.length > 0;
-		// nodes[0].getAttribute("type") == "WorldMorph"; // world is not always serialized
-	},
-
 	setupCounter: function(doc) {
 		var maxCount = new Query('//*[@id]').findAll(doc).inject(0, function(max, ea) {
 			function getNumber(id) { return Number(id.split(':')[0]) }
@@ -88,76 +100,118 @@ Object.subclass('lively.Main.Loader', {
 			WikiNavigator.enableWikiNavigator();
 		});
 	},
+	canvasHeightPatch: function(canvas) {
+	    if (canvas.height && canvas.height.baseVal && canvas.height.baseVal.value < 200) {
+	        // a forced value, some browsers have problems with height=100%
+	        canvas.setAttribute("height", "800");
+	    }
+	},
 
-	systemStart: function(canvas) {
+},
+'loading', {
+
+	systemStart: function(optCanvas) {
 		console.group("World loading");
 
+		var loader = this,
+			importer = new Importer(),
+			canvas = optCanvas || Global.document.getElementById("canvas"),
+			world = null;
+
 		// -----------------
+		this.canvasHeightPatch(canvas);
 		this.configPatches();
 		this.debuggingExtras();
 		this.clipboardHack();
 		this.replaceWindowMorphIfNotExisiting()
 		// ------------------------
 
-	    var world = null;
-	    canvas = canvas || Global.document.getElementById("canvas");
-		var loader = this;
-
-
-	    if (canvas.height && canvas.height.baseVal && canvas.height.baseVal.value < 200) {
-	        // a forced value, some browsers have problems with height=100%
-	        canvas.setAttribute("height", "800");
-	    }
-	    var importer = new Importer();
-
 		Event.prepareEventSystem(canvas);
 
-		if (loader.documentHasSerializedMorphs(document)) {
-			loader.setupCounter(document);
-	        require(Config.modulesBeforeChanges).toRun(function() {
-				var changes = !Config.skipChanges && ChangeSet.fromWorld(document.documentElement);
-				changes && changes.evaluateWorldRequirements();
-				require(Config.modulesBeforeWorldLoad).toRun(function() {
-					changes && changes.evaluateAllButInitializer();
-					require(Config.modulesOnWorldLoad).toRun(function() {
-						world = importer.loadWorldContents(document);
-			            world.displayOnCanvas(canvas);
-			            console.log("world is " + world);
-						changes && changes.evaluateInitializer();
-
-						loader.onFinishLoading(world);
-
-			            if (Config.showWikiNavigator) loader.showWikiNavigator();
-
-						console.groupEnd("World loading");
-					})
-				})
-	        })
-	    } else {
-			// Create an empty world
-			require(Config.modulesBeforeChanges.concat(Config.modulesBeforeWorldLoad).concat(Config.modulesOnWorldLoad)).toRun(function() {
-				world = new WorldMorph(canvas);
-				world.displayOnCanvas(canvas);
-
-				console.log("created empty world");
-				console.groupEnd("World loading");
-
-				loader.browserSpecificFixes();
-
-				if(Config.useShadowMorphs) HandMorph.prototype.useShadowMorphs = true;
-
-				if (!Config.skipAllExamples)
-					new lively.Main.ExampleLoader().populateWorldWithExamples(world);
-
-				loader.onFinishLoading(world);
-			})
-	    }
-
+		var json;
+		if (json = this.findJSONWorld()) {
+			this.deserializeJSONWorld(canvas, json)
+		} else if (loader.documentHasSerializedMorphs(document)) {
+			this.deserializeXMLWorld(canvas);
+		} else {
+			this.createNewWorld(canvas);
+		}
 	},
-onFinishLoading: function(world) {
-	world.hideHostMouseCursor();
-	if (lively.bindings) lively.bindings.signal(this, 'finishLoading', world);
-},
+
+	onFinishLoading: function(world) {
+		console.groupEnd("World loading");
+		world.hideHostMouseCursor();
+		if (lively.bindings) lively.bindings.signal(this, 'finishLoading', world);
+	},
+	deserializeXMLWorld: function(canvas) {
+		var world, importer = new Importer(), self = this;
+		this.setupCounter(canvas);
+        require(Config.modulesBeforeChanges).toRun(function() {
+			var changes = !Config.skipChanges && ChangeSet.fromWorld(canvas);
+			changes && changes.evaluateWorldRequirements();
+			require(Config.modulesBeforeWorldLoad).toRun(function() {
+				changes && changes.evaluateAllButInitializer();
+				require(Config.modulesOnWorldLoad).toRun(function() {
+					world = importer.loadWorldContents(canvas);
+		            world.displayOnCanvas(canvas);
+		            console.log("world is " + world);
+					changes && changes.evaluateInitializer();
+
+					self.onFinishLoading(world);
+
+		            if (Config.showWikiNavigator) self.showWikiNavigator();
+				})
+			})
+        })
+	},
+	deserializeJSONWorld: function(canvas, jsonString) {
+		var self = this;
+		require(["draft.SmartRefSerialization","draft.SerializationRefactoring"].concat(Config.modulesOnWorldLoad)).toRun(function() {
+			var deserializer = SmartRefSerializer.forLively();
+				world = deserializer.deserialize(jsonString);
+			world.displayOnCanvas(canvas)
+
+			self.setupCounter(canvas);
+			if (Config.showWikiNavigator) self.showWikiNavigator();
+			self.onFinishLoading(world);
+			world.setStatusMessage('JSON world loaded', Color.green, 5)
+		})
+        // require(Config.modulesBeforeChanges).toRun(function() {
+			// var changes = !Config.skipChanges && ChangeSet.fromWorld(canvas);
+			// changes && changes.evaluateWorldRequirements();
+			// require(Config.modulesBeforeWorldLoad).toRun(function() {
+				// changes && changes.evaluateAllButInitializer();
+				// require(Config.modulesOnWorldLoad).toRun(function() {
+					// world = importer.loadWorldContents(canvas);
+		            // world.displayOnCanvas(canvas);
+		            // console.log("world is " + world);
+					// changes && changes.evaluateInitializer();
+// 
+// 
+				// })
+			// })
+        // })
+	},
+
+	createNewWorld: function(canvas) {
+		var world,
+			requirements = Config.modulesBeforeChanges
+				.concat(Config.modulesBeforeWorldLoad)
+				.concat(Config.modulesOnWorldLoad);
+
+		require(requirements).toRun(function() {
+			var world = new WorldMorph(canvas);
+			world.displayOnCanvas(canvas);
+
+			loader.browserSpecificFixes();
+
+			if (Config.useShadowMorphs) HandMorph.prototype.useShadowMorphs = true;
+			if (!Config.skipAllExamples)
+				new lively.Main.ExampleLoader().populateWorldWithExamples(world);
+
+			loader.onFinishLoading(world);
+		})
+	},
 
 
 
